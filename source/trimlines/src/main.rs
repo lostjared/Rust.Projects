3d89e566ff587b8b219de21ef1648c947e8c488e
Jared Bruni <jaredbruni@gmail.com>
2021-11-20 21:36:33 -0800

    added new test project

---
+++ source/trimlines/src/main.rs
@@ -0,0 +1,30 @@
+
+use std::env;
+use std::fs;
+use std::fs::File;
+use std::io::Write;
+
+fn trimlines(inputfile: &String, outputfile: &String) {
+    let contents = fs::read_to_string(inputfile).expect("Error reading the file");
+    let mut val : Vec<String> = Vec::new();
+    for i in contents.split("\n") {
+        if i.len() > 0 {
+            val.push(i.to_string());
+        }
+    }
+    let mut cfile = File::create(outputfile).expect("Error creating file");
+    val.iter().for_each(|x| writeln!(&mut cfile, "{}", x).expect("error on write"));
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() == 3 {
+        let val1 = args.get(1).unwrap();
+        let val2 = args.get(2).unwrap();
+        trimlines(val1, val2);
+    }
+    else {
+        println!("Error: input_file output_file");
+    }
+
+}
