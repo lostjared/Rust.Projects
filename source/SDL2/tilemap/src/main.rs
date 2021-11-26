d0292950ab4798b6728680f1fd4b1694c52d257f
Jared Bruni <jaredbruni@gmail.com>
2021-11-25 17:14:25 -0800

    split into seperate module

--- source/SDL2/tilemap/src/main.rs
+++ source/SDL2/tilemap/src/main.rs
@@ -2,85 +2,14 @@
 // followed this tutorial here: https://developer.mozilla.org/en-US/docs/Games/Techniques/Tilemaps
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
-use sdl2::rect::Rect;
 use std::path::Path;
 use sdl2::surface::Surface;
 use std::time::{SystemTime, UNIX_EPOCH};
-use std::cmp::max;
-use std::cmp::min;
+
 use std::collections::HashSet;
 
-struct Camera {
-    x: i32,
-    y: i32,
-    width: i32,
-    height: i32,
-    max_x: i32,
-    max_y: i32,
-    speed: i32
-}
-
-impl Camera {
-    fn new(w: i32, h: i32, mx: i32, my: i32) -> Camera {
-        Camera { 
-            x:0,
-            y:0, 
-            width:w,
-            height:h, 
-            max_x: mx, 
-            max_y: my,
-            speed: 512
-        }
-    }
-    fn move_camera(&mut self, delta: f64, dx: i32, dy: i32) {
-        let dx_val : f64 = dx as f64 * self.speed as f64 * delta;
-        let dy_val : f64 = dy as f64 * self.speed as f64 * delta;
-        self.x += dx_val as i32;
-        self.y += dy_val as i32;
-        self.x = max(0, min(self.x, self.max_x));
-        self.y = max(0, min(self.y, self.max_y));
-    }
-}
-
-struct Map {
-    cols: i32,
-    rows: i32,
-    tsize: i32,
-    layers: Vec<u8>
-}
-
-impl Map {
-    fn new(col: i32, row: i32, ts: i32, l: Vec<u8>) -> Map {
-        Map {
-            cols: col,
-            rows: row,
-            tsize: ts,
-            layers: l
-        }
-    }
-
-    fn draw_map(&self, texture: &sdl2::render::Texture, camera: &Camera, can: &mut sdl2::render::Canvas<sdl2::video::Window>) {
-        let start_col =   camera.x / self.tsize as i32;
-        let end_col    =  start_col + (camera.width / self.tsize as i32);
-        let start_row  =  camera.y / self.tsize as i32;
-        let end_row    =  start_row +  (camera.height / self.tsize as i32);
-        let cx : i32 = camera.x as i32;
-        let cy : i32 = camera.y as i32;
-        let off_x : i32 = -cx + start_col as i32 * self.tsize as i32;
-        let off_y : i32 = -cy + start_row as i32 * self.tsize as i32;
-        for i in start_col ..= end_col {
-            for z in start_row ..= end_row {
-                let tile : u8 = self.layers[(z * self.rows + i) as usize];
-                let x : i32 = (i- start_col) * self.tsize + off_x;
-                let y : i32 = (z- start_row) * self.tsize + off_y;
-                let t : i32 = tile as i32;
-                if t != 0 {
-                    can.copy(&texture, Some(Rect::new((t-1) as i32 * self.tsize as i32, 0, self.tsize as u32, self.tsize as u32)), Some(Rect::new(x as i32, y as i32, self.tsize as u32, self.tsize as u32))).expect("on copy");
-                }
-            }
-        }
-    }
-}
+mod map;
+pub use map::level;
 
 fn fmin(x: f64, x2: f64) -> f64 {
     if x < x2 {
@@ -118,10 +47,10 @@ fn main() {
         3, 3, 3, 1, 1, 2, 3, 3, 3, 3, 3, 3,
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
     ];
-    let map = Map::new(12, 12, 64, tiles);
+    let map = level::Map::new(12, 12, 64, tiles);
     let max_x : i32 = (map.cols * 64 - 512)-1;
     let max_y : i32 = (map.cols * 64 - 512)-1;
-    let mut camera = Camera::new(512, 512, max_x, max_y);
+    let mut camera = level::Camera::new(512, 512, max_x, max_y);
     let mut prev_tick : u64 = 0;
     'main: loop {
         let start = SystemTime::now();
@@ -167,5 +96,5 @@ fn main() {
                 camera.move_camera(delta, move_x, move_y);
                 println!("x: {} y: {} delta: {}", move_x, move_y, delta);
             }
-    }
+   }
 }
