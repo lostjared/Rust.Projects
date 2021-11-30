c808b23ace4727c4ef48f71ee0a30377bcf767cb
Jared Bruni <jaredbruni@gmail.com>
2021-11-30 15:34:39 -0800

    updated code

--- source/command/commandex03/src/main.rs
+++ source/command/commandex03/src/main.rs
@@ -2,9 +2,9 @@
 use std::process::Command;
 
 fn main() {
-    let _ = if cfg!(target_os = "windows") {
-        Command::new("cmd").args(["/C", "echo \"Hello World\""]).spawn().expect("execfile")
+    if cfg!(target_os = "windows") {
+        Command::new("cmd").args(["/C", "echo \"Hello World\""]).spawn().expect("execfile");
     } else {
-        Command::new("sh").arg("-c").arg("echo \"Hello, World\"").spawn().expect("exec file")
-    };
+        Command::new("sh").arg("-c").arg("echo \"Hello, World\"").spawn().expect("exec file");
+    }
 }
