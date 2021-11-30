6e775ac2337f9df53e10db82d118c56b8ea7966b
Jared Bruni <jaredbruni@gmail.com>
2021-11-30 15:31:29 -0800

    added new test

---
+++ source/command/commandex03/src/main.rs
@@ -0,0 +1,10 @@
+
+use std::process::Command;
+
+fn main() {
+    let _ = if cfg!(target_os = "windows") {
+        Command::new("cmd").args(["/C", "echo \"Hello World\""]).spawn().expect("execfile")
+    } else {
+        Command::new("sh").arg("-c").arg("echo \"Hello, World\"").spawn().expect("exec file")
+    };
+}
