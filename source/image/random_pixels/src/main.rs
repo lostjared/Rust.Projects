a22110f192c8c541f33bf6b8c5edfcc1bbbf9fe2
Jared Bruni <jaredbruni@gmail.com>
2021-12-01 17:27:31 -0800

    added image example

---
+++ source/image/random_pixels/src/main.rs
@@ -0,0 +1,14 @@
+extern crate image;
+use rand::Rng;
+
+fn main() {
+    let mut random = rand::thread_rng();
+    let mut img = image::ImageBuffer::new(1920, 1080);
+    for (_x, _y, pixel) in img.enumerate_pixels_mut() {
+        let r : u8 = random.gen();
+        let g : u8 = random.gen();
+        let b : u8 = random.gen();
+        *pixel = image::Rgb([r, g, b]);
+    }
+    img.save("image.png").unwrap();
+}
