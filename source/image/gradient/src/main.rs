4f0db4756606dbce766904fc052e3b245f0633f1
Jared Bruni <jaredbruni@gmail.com>
2021-12-01 21:42:57 -0800

    added image gradient example

---
+++ source/image/gradient/src/main.rs
@@ -0,0 +1,20 @@
+extern crate image;
+
+fn main() {
+  let mut img = image::ImageBuffer::new(256, 256);
+  for (_x, _y, pixel) in img.enumerate_pixels_mut() {
+        let r : u8 = 0;
+        let g : u8 = 0;
+        let b : u8 = 0;
+        *pixel = image::Rgb([r, g, b]);
+    }
+    for y in 0..256 {
+        for x in 0..256 {
+            let pixel = img.get_pixel_mut(x, y);
+            let image::Rgb(data) = *pixel;
+            *pixel = image::Rgb([x as u8,y as u8,0]);
+        }
+    
+    }
+    img.save("image.png").unwrap();
+}
