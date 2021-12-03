752bbf0dab126f78e4fb3bfc61579f8b64a11fe1
Jared Bruni <jaredbruni@gmail.com>
2021-12-03 14:07:33 -0800

    got it working right

--- source/image/proc_image_par/src/main.rs
+++ source/image/proc_image_par/src/main.rs
@@ -11,10 +11,12 @@ fn main() -> std::io::Result<()> {
     println!("{}x{}", img.width(), img.height());
     let values : Vec<(usize, &mut [u8])> = bytes.chunks_mut(img.width() as usize * 4 as usize).enumerate().collect();
     values.into_par_iter().for_each(|(_u, b)| {
-        let mut i : f32 = 1.0;
-        for q in b {
-            *q = ((i as f32 * 0.5) + (*q as f32 * 0.5)) as u8;
-            i += 0.1;
+        let mut y : usize = 0;
+        let len = b.len()-1;
+        while y < len {
+            b[y+1] = ((y as f32 * 0.3 ) + (0.5 * b[y+1] as f32)) as u8;
+            b[y+2] = (0.5 * b[y+2] as f32) as u8;
+            y += 4;
         }
     });
     image::save_buffer("output.png", &mut bytes , img.width(), img.height(), image::ColorType::Rgba8).unwrap();
