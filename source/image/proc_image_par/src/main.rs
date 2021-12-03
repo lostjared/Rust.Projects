04819e09cc00d43e9e45e528c29f72a724532e42
Jared Bruni <jaredbruni@gmail.com>
2021-12-03 13:45:45 -0800

    Updated code

--- source/image/proc_image_par/src/main.rs
+++ source/image/proc_image_par/src/main.rs
@@ -1,22 +1,20 @@
 // manipulate pixels
 extern crate image;
-use std::env;
 use image::io::Reader as ImageReader;
-use image::{GenericImage, GenericImageView};
+use image::*;
 use rayon::prelude::*;
 
 fn main() -> std::io::Result<()> {
-    let args: Vec<String> = env::args().collect();
-    if args.len() != 2 {
-        println!("Error ... requires filename");
-        return Ok(());
-    }
-    let img = ImageReader::open(args.get(1).unwrap())?.decode().unwrap();
+
+    let img = ImageReader::open("image.png")?.decode().unwrap();
     let mut bytes : Vec<u8>  = Vec::from(img.as_bytes());
-    let values : Vec<(usize, &mut [u8])> = bytes.chunks_mut(img.width() as usize).enumerate().collect();
-    values.into_par_iter().for_each(|(u, b)| {
-        for pix in b {
-            *pix = ((u as f32 * 0.2) + (0.5 * *pix as f32)) as u8;
+    println!("{}x{}", img.width(), img.height());
+    let values : Vec<(usize, &mut [u8])> = bytes.chunks_mut(img.width() as usize * 4 as usize).enumerate().collect();
+    values.into_par_iter().for_each(|(_u, b)| {
+        let mut i : f32 = 1.0;
+        for q in b {
+            *q = ((i as f32 * 0.5) + (*q as f32 * 0.5)) as u8;
+            i += 0.1;
         }
     });
     image::save_buffer("output.png", &mut bytes , img.width(), img.height(), image::ColorType::Rgba8).unwrap();
