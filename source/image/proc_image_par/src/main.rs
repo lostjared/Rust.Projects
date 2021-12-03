19fc68701001aee33ad7e7c1c3a3f043f29eee1e
Jared Bruni <jaredbruni@gmail.com>
2021-12-03 14:09:09 -0800

    changed variable name

--- source/image/proc_image_par/src/main.rs
+++ source/image/proc_image_par/src/main.rs
@@ -11,12 +11,12 @@ fn main() -> std::io::Result<()> {
     println!("{}x{}", img.width(), img.height());
     let values : Vec<(usize, &mut [u8])> = bytes.chunks_mut(img.width() as usize * 4 as usize).enumerate().collect();
     values.into_par_iter().for_each(|(_u, b)| {
-        let mut y : usize = 0;
+        let mut x : usize = 0;
         let len = b.len()-1;
-        while y < len {
-            b[y+1] = ((y as f32 * 0.3 ) + (0.5 * b[y+1] as f32)) as u8;
-            b[y+2] = (0.5 * b[y+2] as f32) as u8;
-            y += 4;
+        while x < len {
+            b[x+1] = ((x as f32 * 0.3 ) + (0.5 * b[x+1] as f32)) as u8;
+            b[x+2] = (0.5 * b[x+2] as f32) as u8;
+            x += 4;
         }
     });
     image::save_buffer("output.png", &mut bytes , img.width(), img.height(), image::ColorType::Rgba8).unwrap();
