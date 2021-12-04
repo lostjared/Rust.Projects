0ece80ebd067b98b7d86d371aab408a20ae235d7
Jared Bruni <jaredbruni@gmail.com>
2021-12-04 15:05:39 -0800

    added tuple test to help me remember

---
+++ source/language/tuple/src/main.rs
@@ -0,0 +1,23 @@
+
+fn values(one: u32) -> (u32, u32, f32) {
+    if one == 1 {
+        (10, 10, 10.5)
+    } else {
+        (5, 5, 5.5)
+    }
+}
+
+fn print_values(x : (u32, u32, u32, u32)) {
+    println!("{}: {}: {}: {}", x.0, x.1, x.2, x.3);
+}
+
+fn main() {
+    let val = values(1);
+    let val2 = values(2);
+    println!("{}: {}: {}", val.0, val.1, val.2);
+    println!("{}: {}: {}", val2.0, val2.1, val2.2);
+    let val3 = (20, 20, 20.5);
+    println!("{}: {}: {}", val3.0, val3.1, val3.2);
+    print_values((0, 1, 2, 3));
+}
+
