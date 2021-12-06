8978fb78e70dbf1558b811a675c6d88288028994
Jared Bruni <jaredbruni@gmail.com>
2021-12-05 19:31:35 -0800

    working on the game grid

--- source/puzzle_game/src/main.rs
+++ source/puzzle_game/src/main.rs
@@ -1,32 +1,43 @@
 
 // cargo run --release
 
+mod puzzle;
+
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::PixelFormatEnum;
 use sdl2::rect::Rect;
 use sdl2::pixels::Color;
 use rand::Rng;
-mod puzzle;
 use puzzle::game;
 
 fn draw_grid(grid : &game::Grid, colors: &Vec<Color>, can: &mut sdl2::render::Canvas<sdl2::video::Window>) {
-    let mut rng = rand::thread_rng();
     let offset = 10;
     for x in 0..grid.get_width() as usize {
         for y in 0..grid.get_height() as usize {
-            let mut color = grid.get_grid_point(x, y);
-            color = rng.gen_range(0..7); // for now
+            let color = grid.get_grid_point(x, y);
             let value: Color = *colors.get(color as usize).unwrap();
             can.set_draw_color(value);
             can.fill_rect(Some(Rect::new(x as i32 * 32, (y as i32 * 16) + offset, 32, 16))).expect("draw rect");
         }
     }
+
+    let block = grid.get_block();
+    let mut value: Color = *colors.get(block[0].color as usize).unwrap();
+    can.set_draw_color(value);
+    can.fill_rect(Some(Rect::new(block[0].x as i32 * 32, (block[0].y as i32 * 16) + offset, 32, 16))).expect("draw rect");
+    value = *colors.get(block[1].color as usize).unwrap();
+    can.set_draw_color(value);
+    can.fill_rect(Some(Rect::new(block[1].x as i32 * 32, (block[1].y as i32 * 16) + offset, 32, 16))).expect("draw rect");
+    value = *colors.get(block[2].color as usize).unwrap();
+    can.set_draw_color(value);
+    can.fill_rect(Some(Rect::new(block[2].x as i32 * 32, (block[2].y as i32 * 16) + offset, 32, 16))).expect("draw rect");
 }
 
 fn main() {   
-    let width = 1920;
-    let height = 1080;
+
+    let width = 1280;
+    let height = 720;
     let sdl = sdl2::init().unwrap();
     let video = sdl.video().unwrap();
     let window = video.window("Generic Puzzle Game", width, height).resizable().opengl().build().unwrap();
@@ -37,9 +48,10 @@ fn main() {
     can.set_draw_color(Color::RGB(0, 0, 0));
     can.clear();
     can.present();
-    let grid : game::Grid = game::Grid::new(1920/32, 1080/16);
+    let mut grid : game::Grid = game::Grid::new(1280/32, 720/16);
+    grid.new_piece();
     let mut colors = vec![];
-    colors.push(Color::RGB(255, 0, 0));
+    colors.push(Color::RGB(0, 0, 0));
     colors.push(Color::RGB(0, 255, 0));
     colors.push(Color::RGB(0, 0, 255));
     colors.push(Color::RGB(255, 255, 0));
@@ -48,6 +60,8 @@ fn main() {
     colors.push(Color::RGB(255, 0, 255,));
     colors.push(Color::RGB(150, 155, 40));
     colors.push(Color::RGB(50, 150, 200));
+    colors.push(Color::RGB(255, 0, 0));
+
     'main: loop {
         for _event in e.poll_iter() {
             match _event {
@@ -56,19 +70,18 @@ fn main() {
                     keycode: Some(Keycode::Escape),
                     ..
                 } => break 'main,
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Left),
+                    ..
+                } => grid.move_left(),
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Right),
+                    ..
+                } => grid.move_right(),
+                |
                 _ => {}
             }
         }
-        texture.with_lock(None, |buffer: &mut [u8], pitch: usize| {
-           for y in 0..height {
-                for x in 0..width {
-                    let offset = y  * pitch as u32 + x * 3;
-                    buffer[offset as usize] = 0;
-                    buffer[offset as usize + 1] = 0;
-                    buffer[offset as usize + 2] = 0;
-                }
-            }
-        }).expect("on lock");
         can.set_draw_color(Color::RGB(0, 0, 0));
         can.clear();
         draw_grid(&grid,&colors,&mut can);
--- source/puzzle_game/src/puzzle.rs
+++ source/puzzle_game/src/puzzle.rs
@@ -2,36 +2,62 @@
 
 pub mod game {
 
+    use rand::Rng;
+
     #[derive(Copy,Clone,Debug)]
     pub struct Block {
-        x: i32,
-        y: i32,
-        color: i32,
+        pub x: i32,
+        pub y: i32,
+        pub color: i32,
     }
 
-    const TILE_W: usize = 1920/32;
-    const TILE_H: usize = 1080/16;
+    const TILE_W: usize = 1280/32;
+    const TILE_H: usize = 720/16;
 
     pub struct Grid {
         blocks: Box<[[Block; TILE_H]; TILE_W]>,
         width: i32,
         height: i32,
+        piece: [Block; 3]
     }
 
     impl Block {
         fn new(xx: i32, xy: i32, xcolor: i32) -> Block {
             Block {x: xx, y: xy, color: xcolor}
         }
+
     }
 
     impl Grid {
         pub fn new(widthx: i32, heightx: i32) -> Grid {
             let g = Box::new([[Block{x: 0, y: 0, color: 0}; TILE_H]; TILE_W]);
-            Grid{ blocks: g, width: widthx, height: heightx }
+            Grid{ blocks: g, width: widthx, height: heightx, piece: [Block { x: 0, y: 0, color: 0}; 3] }
+        }
+
+        pub fn new_piece(&mut self) {
+            let mut rng = rand::thread_rng();
+            self.piece[0].x = (TILE_W/2) as i32;
+            self.piece[0].y = 0;
+            self.piece[0].color = rng.gen_range(1..8);
+            self.piece[1].x = (TILE_W/2) as i32;
+            self.piece[1].y = 1;
+            self.piece[1].color = rng.gen_range(1..8);
+            self.piece[2].x = (TILE_W/2) as i32;
+            self.piece[2].y = 2;
+            self.piece[2].color = rng.gen_range(1..8);
+            while self.piece[0].color == self.piece[1].color || self.piece[0].color == self.piece[2].color {
+                self.piece[0].color = rng.gen_range(1..8);
+                self.piece[1].color = rng.gen_range(1..8);
+                self.piece[2].color = rng.gen_range(1..8);
+            }
         }
 
         pub fn get_grid_point(&self, x: usize, y: usize) -> i32 {
             self.blocks[x][y].color
+        }
+
+        pub fn get_block(&self) -> [Block; 3] {
+            self.piece
         }
 
         pub fn get_width(&self) -> i32 {
@@ -40,6 +66,34 @@ pub mod game {
 
         pub fn get_height(&self) -> i32 {
             self.height
+        }
+        
+        pub fn move_left(&mut self) {
+            let mut go = true;
+            for i in 0..3 {
+                if self.piece[i].x <= 0 {
+                    go = false;
+                }
+            }
+            if go == true {
+                for i in 0..3 {
+                    self.piece[i].x -= 1;
+                }
+            }
+        }
+
+        pub fn move_right(&mut self) {
+            let mut go = true;
+            for i in 0..3 {
+                if self.piece[i].x >= (TILE_W as i32)-1 {
+                    go = false;
+                }
+            }
+            if go == true {
+                for i in 0..3 {
+                    self.piece[i].x += 1;
+                }
+            }
         }
     }
 
