f30ab672e562b8e9793eb2537e4fbc5548f524b7
Jared Bruni <jaredbruni@gmail.com>
2021-12-16 04:12:20 -0800

    formatting

--- source/SDL2/text/src/main.rs
+++ source/SDL2/text/src/main.rs
@@ -9,13 +9,25 @@ fn main() {
     let height = 720;
     let sdl = sdl2::init().unwrap();
     let video = sdl.video().unwrap();
-    let window = video.window("App", width, height).resizable().opengl().build().unwrap();
-    let mut can = window.into_canvas().build().map_err(|e| e.to_string()).expect("Error on canvas");
+    let window = video
+        .window("App", width, height)
+        .resizable()
+        .opengl()
+        .build()
+        .unwrap();
+    let mut can = window
+        .into_canvas()
+        .build()
+        .map_err(|e| e.to_string())
+        .expect("Error on canvas");
     let ttf_context = sdl2::ttf::init().map_err(|e| e.to_string()).unwrap();
     let font = ttf_context.load_font("./font.ttf", 18).expect("test");
     let tc = can.texture_creator();
-    let text_surf = font.render("Hello, World!").blended(Color::RGB(255,255,255)).unwrap();
-    let text_surf_tex = tc.create_texture_from_surface(&text_surf).unwrap();        
+    let text_surf = font
+        .render("Hello, World!")
+        .blended(Color::RGB(255, 255, 255))
+        .unwrap();
+    let text_surf_tex = tc.create_texture_from_surface(&text_surf).unwrap();
     let mut e = sdl.event_pump().unwrap();
     'main: loop {
         for _event in e.poll_iter() {
@@ -28,10 +40,20 @@ fn main() {
                 _ => {}
             }
         }
-        can.set_draw_color(Color::RGB(0,0,0));
+        can.set_draw_color(Color::RGB(0, 0, 0));
         can.clear();
-        let TextureQuery { width : wi, height: hi, .. } = text_surf_tex.query();
-        can.copy(&text_surf_tex, Some(Rect::new(0, 0, wi, hi)), Some(Rect::new(25, 25, wi, hi))).expect("on font copy");
+        let tc = can.texture_creator();
+        let TextureQuery {
+            width: wi,
+            height: hi,
+            ..
+        } = text_surf_tex.query();
+        can.copy(
+            &text_surf_tex,
+            Some(Rect::new(0, 0, wi, hi)),
+            Some(Rect::new(25, 25, wi, hi)),
+        )
+        .expect("on font copy");
         can.present();
     }
 }
