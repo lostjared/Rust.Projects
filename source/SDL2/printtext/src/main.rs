887fa6321b6e7695ca9395be479387a1b478b5c5
Jared Bruni <jaredbruni@gmail.com>
2021-12-18 07:21:32 -0800

    added cursor

--- source/SDL2/printtext/src/main.rs
+++ source/SDL2/printtext/src/main.rs
@@ -26,12 +26,12 @@ fn printtext(can: &mut sdl2::render::Canvas<sdl2::video::Window>, tex: &sdl2::re
     .expect("on font copy");
 }
 
-fn printtext_width(can: &mut sdl2::render::Canvas<sdl2::video::Window>, tex: &sdl2::render::TextureCreator<sdl2::video::WindowContext>, font: &sdl2::ttf::Font, x: i32, y: i32, w: u32, color: sdl2::pixels::Color, text: &str) {
+fn printtext_width(blink: bool, can: &mut sdl2::render::Canvas<sdl2::video::Window>, tex: &sdl2::render::TextureCreator<sdl2::video::WindowContext>, font: &sdl2::ttf::Font, x: i32, y: i32, w: u32, color: sdl2::pixels::Color, text: &str) {
 
     let mut vlst : Vec<String>  = Vec::new();
     let mut width = x;
     let metrics = font.find_glyph_metrics('A').unwrap();
-    //let mut ypos = y;
+    let mut ypos = y;
 
     let mut value = String::new();
 
@@ -39,7 +39,7 @@ fn printtext_width(can: &mut sdl2::render::Canvas<sdl2::video::Window>, tex: &sd
         if (width + metrics.advance > (w-25) as i32) || ch == '\n' {
             vlst.push(value);
             value = String::new();
-    //      ypos += metrics.advance+metrics.maxy;
+            ypos += metrics.advance+metrics.maxy;
             width = x;
         } else {
             value.push(ch);
@@ -58,6 +58,10 @@ fn printtext_width(can: &mut sdl2::render::Canvas<sdl2::video::Window>, tex: &sd
         }
     }
 
+    if blink == true {
+        can.set_draw_color(color);
+        can.fill_rect(Rect::new(width+5, ypos, 8, (metrics.maxy+metrics.advance) as u32)).expect("failed on rect");
+    }
 }
 
 fn main() {
@@ -84,6 +88,7 @@ fn main() {
         .blended(Color::RGB(255, 255, 255))
         .unwrap();
     let mut e = sdl.event_pump().unwrap();
+    let mut flash = 0;
     'main: loop {
         for _event in e.poll_iter() {
             match _event {
@@ -97,7 +102,15 @@ fn main() {
         }
         can.set_draw_color(Color::RGB(0, 0, 0));
         can.clear();
-        printtext_width(&mut can, &tc, &font, 25, 25, width, Color::RGB(255, 255, 255), "Hello, World with SDL2/TTF!\nLine Two\nLine Three");
+        flash += 1;
+        let flash_on;
+        if flash>10 {
+            flash_on = true;
+            flash = 0;
+        } else {
+            flash_on = false;
+        }
+        printtext_width(flash_on, &mut can, &tc, &font, 25, 25, width, Color::RGB(255, 255, 255), "Hello, World with SDL2/TTF!\nLine Two\nLine Three");
         can.present();
     }
 }
