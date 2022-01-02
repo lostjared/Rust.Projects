709a8603b54d727810393ca4c849cd5889c025e9
Jared Bruni <jaredbruni@gmail.com>
2022-01-02 14:06:18 -0800

    updated added require args

--- source/cmd-switch/src/bin/main.rs
+++ source/cmd-switch/src/bin/main.rs
@@ -5,15 +5,11 @@ use std::env;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let mut desc = HashMap::new();
-    desc.insert(String::from("hello"), String::from("enter your name"));
-    cmd_sw::print_accepted_args(&desc);
-    let argz = cmd_sw::parse_args(&args, &desc);
+    desc.insert(String::from("hello"), (String::from("enter your name"), true));
+    //cmd_sw::print_accepted_args(&desc);
+    let argz = cmd_sw::parse_args_require(&args, &desc);
     for (key, value) in &argz {
         println!("{} = {}", *key, value.value);
-    }
-    if !argz.contains_key("hello") {
-        println!("Requires --hello=name ");
-        std::process::exit(0);
     }
     let v = argz.get("hello").unwrap();
     println!("Key is {} value is {} its for {}", v.key, v.value, v.desc);
--- source/cmd-switch/src/lib.rs
+++ source/cmd-switch/src/lib.rs
@@ -14,7 +14,7 @@ pub mod cmd_sw {
             Argument {
                 key: String::from(k),
                 value: String::from(v),
-                desc: String::from(d)
+                desc: String::from(d),
             }
         }
     }
@@ -54,6 +54,42 @@ pub mod cmd_sw {
                 argz.insert(String::from(key), Argument::new(key,right,&d));
             } else {
                 //println!("Incorrect format: use --key=value found: {}", i);
+            }
+        }
+        argz
+    }
+
+    pub fn parse_args_require(args: &Vec<String>, desc: &HashMap<String, (String, bool)>) -> HashMap<String, Argument> {
+        let mut argz : HashMap<String, Argument> = HashMap::new();
+        let mut arg_req : HashMap<String, bool> = HashMap::new();
+        for i in args.into_iter().skip(1) {
+            let pos = i.find("=");
+            let pos_s = i.find("--");
+            if pos != None && pos_s != None {
+                let loc = (pos_s.unwrap(), pos.unwrap());
+                let key = &i[loc.0+2..loc.1];
+                let right = &i[loc.1+1..i.len()];
+                let d;
+                let s = desc.get(key);
+                if s != None {
+                    let val = s.unwrap();
+                    d = String::from(&val.0);
+                    let d2 = String::from(key);
+                    arg_req.insert(d2, true);
+                } else {
+                    d = String::from("None");
+                }
+                argz.insert(String::from(key), Argument::new(key,right,&d));
+            } else {
+                //println!("Incorrect format: use --key=value found: {}", i);
+            }
+        }
+
+        for (key, value) in desc {
+            if value.1 == true {
+                if !arg_req.contains_key(key) {
+                    panic!("Error required argument {} missing: [{}]\n", key, value.0);
+                } 
             }
         }
         argz
