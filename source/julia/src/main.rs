9121ce6ffbcef41132736768313f74711178d04f
Jared Bruni <jaredbruni@gmail.com>
2022-01-03 09:55:48 -0800

    formatting

--- source/julia/src/main.rs
+++ source/julia/src/main.rs
@@ -11,7 +11,7 @@ pub fn parse_split_int(t: &str) -> (i32, i32) {
     }
     let pos_value = pos.unwrap();
     let left = &text[0..pos_value];
-    let right = &text[pos_value+1..text.len()];
+    let right = &text[pos_value + 1..text.len()];
     (left.parse::<i32>().unwrap(), right.parse::<i32>().unwrap())
 }
 
@@ -23,7 +23,7 @@ pub fn parse_split_double(t: &str) -> (f32, f32) {
     }
     let pos_value = pos.unwrap();
     let left = &text[0..pos_value];
-    let right = &text[pos_value+1..text.len()];
+    let right = &text[pos_value + 1..text.len()];
     (left.parse::<f32>().unwrap(), right.parse::<f32>().unwrap())
 }
 
@@ -32,10 +32,15 @@ fn main() {
     let mut desc = HashMap::new();
 
     desc.insert(String::from("output"), (String::from("output file"), true));
-    desc.insert(String::from("res"), (String::from("output resolution"), true));
+    desc.insert(
+        String::from("res"),
+        (String::from("output resolution"), true),
+    );
     desc.insert(String::from("param"), (String::from("complex pair"), true));
-    desc.insert(String::from("iter"), (String::from("number of iterations"), true));
-
+    desc.insert(
+        String::from("iter"),
+        (String::from("number of iterations"), true),
+    );
 
     if args.len() == 1 {
         cmd_sw::print_accepted_args_map_require(&desc);
@@ -51,14 +56,14 @@ fn main() {
     let iterations = iter.value.parse::<i32>().unwrap();
     let params = parse_split_double(&param.value);
 
-    println!("output: [{}] resolution: ({}, {}) param: ({}, {}) iterations: {}", output.value, res_value.0, res_value.1, params.0, params.1, iterations);
+    println!(
+        "output: [{}] resolution: ({}, {}) param: ({}, {}) iterations: {}",
+        output.value, res_value.0, res_value.1, params.0, params.1, iterations
+    );
     draw_julia(&output.value, res_value, params, iterations);
-
 }
 
-
 pub fn draw_julia(filename: &String, res: (i32, i32), param: (f32, f32), iter: i32) {
-
     // from image crate example website
     let imgx = res.0 as u32;
     let imgy = res.1 as u32;
@@ -88,5 +93,4 @@ pub fn draw_julia(filename: &String, res: (i32, i32), param: (f32, f32), iter: i
         }
     }
     imgbuf.save(&filename).unwrap();
-
 }
