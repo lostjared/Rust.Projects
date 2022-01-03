0e60f6b952046d2a5d1538246f1c512074b771c4
Jared Bruni <jaredbruni@gmail.com>
2022-01-03 09:55:30 -0800

    julia

--- source/julia/src/main.rs
+++ source/julia/src/main.rs
@@ -1,7 +1,7 @@
 use cmd_switch::cmd_sw;
 use std::collections::HashMap;
 use std::env;
-
+extern crate image;
 
 pub fn parse_split_int(t: &str) -> (i32, i32) {
     let text = String::from(t.trim());
@@ -56,7 +56,37 @@ fn main() {
 
 }
 
+
 pub fn draw_julia(filename: &String, res: (i32, i32), param: (f32, f32), iter: i32) {
 
+    // from image crate example website
+    let imgx = res.0 as u32;
+    let imgy = res.1 as u32;
+
+    let scalex = 3.0 / imgx as f32;
+    let scaley = 3.0 / imgy as f32;
+    let mut imgbuf = image::ImageBuffer::new(imgx, imgy);
+    for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
+        let r = (0.3 * y as f32) as u8;
+        let g = (0.3 * x as f32) as u8;
+        *pixel = image::Rgb([r, g, 0]);
+    }
+    for x in 0..imgx {
+        for y in 0..imgy {
+            let cx = y as f32 * scalex - 1.5;
+            let cy = x as f32 * scaley - 1.5;
+            let c = num::complex::Complex::new(param.0, param.1);
+            let mut z = num::complex::Complex::new(cx, cy);
+            let mut i = 0;
+            while i < iter && z.norm() <= 2.0 {
+                z = z * z + c;
+                i += 1;
+            }
+            let pixel = imgbuf.get_pixel_mut(x, y);
+            let image::Rgb(data) = *pixel;
+            *pixel = image::Rgb([data[0], i as u8, data[2]]);
+        }
+    }
+    imgbuf.save(&filename).unwrap();
 
 }
