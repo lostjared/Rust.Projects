b0295243a41dc4f36aa547cf05e4267bcbb7d594
Jared Bruni <jaredbruni@gmail.com>
2022-01-08 00:27:57 -0800

    proper way

--- source/cmd-switch/src/lib.rs
+++ source/cmd-switch/src/lib.rs
@@ -111,7 +111,7 @@ pub mod cmd_sw {
                 } else {
                     key = &i[loc.0 + 1..loc.1];
                 }
-                let right = &i[loc.1 + 1..i.len()];
+                let right = &i[loc.1 + 1..];
                 let d;
                 let s = desc.get(key);
                 if s != None {
@@ -125,7 +125,7 @@ pub mod cmd_sw {
                 argz.insert(String::from(key), Argument::new(key, right, &d));
             } else if pos_s != None && pos == None {
                 let loc = pos_s.unwrap();
-                let k = &i[loc + 2..i.len()];
+                let k = &i[loc + 2..];
                 let v = desc.get(k);
                 let d;
                 if v != None {
