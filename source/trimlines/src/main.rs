592839b05e752c05f0d88b6d16d5bd2918e927e5
Jared Bruni <jaredbruni@gmail.com>
2022-01-15 13:29:00 -0800

    formatting

--- source/trimlines/src/main.rs
+++ source/trimlines/src/main.rs
@@ -1,4 +1,3 @@
-
 use std::env;
 use std::fs;
 use std::fs::File;
@@ -6,7 +5,7 @@ use std::io::Write;
 
 fn trimlines(inputfile: &String, outputfile: &String) {
     let contents = fs::read_to_string(inputfile).expect("Error reading the file");
-    let val : Vec<&str> = contents.lines().collect();
+    let val: Vec<&str> = contents.lines().collect();
     let mut cfile = File::create(outputfile).expect("Error creating file");
     val.iter().for_each(|x| {
         if x.len() > 0 {
@@ -21,9 +20,7 @@ fn main() {
         let val1 = args.get(1).unwrap();
         let val2 = args.get(2).unwrap();
         trimlines(val1, val2);
-    }
-    else {
+    } else {
         println!("Error: input_file output_file");
     }
-
 }
