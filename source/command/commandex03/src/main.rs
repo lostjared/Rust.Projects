7f75e33806df7186dfc02377ed0d07e204a9e699
Jared Bruni <jaredbruni@gmail.com>
2022-01-15 13:29:44 -0800

    formatting

--- source/command/commandex03/src/main.rs
+++ source/command/commandex03/src/main.rs
@@ -1,10 +1,16 @@
-
 use std::process::Command;
 
 fn main() {
     if cfg!(target_os = "windows") {
-        Command::new("cmd").args(["/C", "echo \"Hello World\""]).spawn().expect("execfile");
+        Command::new("cmd")
+            .args(["/C", "echo \"Hello World\""])
+            .spawn()
+            .expect("execfile");
     } else {
-        Command::new("sh").arg("-c").arg("echo \"Hello, World\"").spawn().expect("exec file");
+        Command::new("sh")
+            .arg("-c")
+            .arg("echo \"Hello, World\"")
+            .spawn()
+            .expect("exec file");
     }
 }
