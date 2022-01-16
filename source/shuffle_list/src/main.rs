567c2d3d0460b5a39b9e12de4b0d5b058bd9a026
Jared Bruni <jaredbruni@gmail.com>
2022-01-15 22:32:08 -0800

    formatting

--- source/shuffle_list/src/main.rs
+++ source/shuffle_list/src/main.rs
@@ -1,14 +1,13 @@
-
 extern crate rand;
+use rand::thread_rng;
+use shuffle::irs::Irs;
+use shuffle::shuffler::Shuffler;
 use std::env;
 use std::fs;
-use rand::thread_rng;
-use shuffle::shuffler::Shuffler;
-use shuffle::irs::Irs;
 
 fn shuffle_list(input: &str) {
     let contents = fs::read_to_string(input).expect("Error reading the file");
-    let mut lines_list : Vec<&str> = contents.lines().collect();
+    let mut lines_list: Vec<&str> = contents.lines().collect();
     let mut rng = thread_rng();
     let mut irs = Irs::default();
     irs.shuffle(&mut lines_list, &mut rng).expect("on shuffle");
@@ -18,7 +17,7 @@ fn shuffle_list(input: &str) {
 }
 
 fn main() {
-    let args : Vec<String> = env::args().collect();
+    let args: Vec<String> = env::args().collect();
     if args.len() <= 1 {
         println!("Error requires argument of list file");
         std::process::exit(-1);
