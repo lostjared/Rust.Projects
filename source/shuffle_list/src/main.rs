2b2f1a176ad45338a3016067067826d98200b111
Jared Bruni <jaredbruni@gmail.com>
2022-01-15 20:34:47 -0800

    added list shuffle

---
+++ source/shuffle_list/src/main.rs
@@ -0,0 +1,31 @@
+
+extern crate rand;
+use std::env;
+use std::fs;
+use rand::thread_rng;
+use shuffle::shuffler::Shuffler;
+use shuffle::irs::Irs;
+
+fn shuffle_list(input: &str) {
+    let contents = fs::read_to_string(input).expect("Error reading the file");
+    let lines = contents.lines();
+    let mut lines_list : Vec<&str> = Vec::new();
+    for i in lines {
+        lines_list.push(i);
+    }
+    let mut rng = thread_rng();
+    let mut irs = Irs::default();
+    irs.shuffle(&mut lines_list, &mut rng).expect("on shuffle");
+    for i in &lines_list {
+        println!("{}", i);
+    }
+}
+
+fn main() {
+    let args : Vec<String> = env::args().collect();
+    if args.len() <= 1 {
+        println!("Error requires argument of list file");
+        std::process::exit(-1);
+    }
+    shuffle_list(&args[1]);
+}
