17b3cb2d9a56df4310093e8b8ffca479486dea23
Jared Bruni <jaredbruni@gmail.com>
2022-01-20 12:32:44 -0800

    updated

--- source/config_file/src/bin/main.rs
+++ source/config_file/src/bin/main.rs
@@ -8,10 +8,22 @@ fn main() {
  //   config.setkey("id","two","value2");
  //   config.save();
     config.load();
-    let value = config.getkey("id", "one");
+    let value = config.getkey("program", "ran");
     match value {
         Some(v) => {
             println!("The value is: {}", v);
+            let run = v.parse::<i32>();
+            match run {
+                Ok(r) => {
+                    let ran = r+1;
+                    config.setkey("program", "ran",&format!("{}", ran));
+                    println!("Program ran: {} ", ran);
+                    config.save();
+                }
+                Err(e) => {
+                    println!("Error converting data: {}...\n", e);
+                }
+            }
         }
         None => {
             println!("value not found!\n");
