985be4a4d2e2c206f2f0ce8fa37b80249aa05f3d
Jared Bruni <jaredbruni@gmail.com>
2022-01-30 20:52:02 -0800

    clippy + fmt

--- source/library/map/src/bin/tilemap.rs
+++ source/library/map/src/bin/tilemap.rs
@@ -1,13 +1,13 @@
 // cargo run --bin tilemap
 // followed this tutorial here: https://developer.mozilla.org/en-US/docs/Games/Techniques/Tilemaps
-use sdl2::event::Event;
-use sdl2::keyboard::Keycode;
-use std::path::Path;
-use sdl2::surface::Surface;
-use std::time::{SystemTime, UNIX_EPOCH};
-use std::collections::HashSet;
 use map::level::Camera;
 use map::level::Map;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::surface::Surface;
+use std::collections::HashSet;
+use std::path::Path;
+use std::time::{SystemTime, UNIX_EPOCH};
 
 fn fmin(x: f64, x2: f64) -> f64 {
     if x < x2 {
@@ -23,38 +23,39 @@ fn main() {
     let height = 512;
     let sdl = sdl2::init().unwrap();
     let video = sdl.video().unwrap();
-    let window = video.window("Tilemap App", width, height).resizable().opengl().build().unwrap();
-    let mut can = window.into_canvas().build().map_err(|e| e.to_string()).expect("Error on canvas");
+    let window = video
+        .window("Tilemap App", width, height)
+        .resizable()
+        .opengl()
+        .build()
+        .unwrap();
+    let mut can = window
+        .into_canvas()
+        .build()
+        .map_err(|e| e.to_string())
+        .expect("Error on canvas");
     let tc = can.texture_creator();
     let surf = Surface::load_bmp(pathval).unwrap();
     let texture = tc.create_texture_from_surface(surf).unwrap();
     let mut e = sdl.event_pump().unwrap();
     let tiles = vec![
-        3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
-        3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 3,
-        3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 3,
-        3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 3,
-        3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 3,
-        3, 1, 1, 1, 1, 1, 2, 1, 1, 1, 3, 3,
-        3, 1, 2, 2, 1, 1, 1, 1, 1, 1, 3, 3,
-        3, 1, 2, 2, 1, 1, 1, 1, 1, 1, 3, 3,
-        3, 1, 1, 1, 2, 1, 1, 1, 1, 1, 3, 3,
-        3, 1, 1, 1, 1, 2, 1, 1, 1, 1, 3, 3,
-        3, 1, 1, 1, 1, 2, 1, 1, 1, 1, 3, 3,
-        3, 3, 3, 1, 1, 2, 3, 3, 3, 3, 3, 3,
-        3, 3, 3, 1, 1, 2, 3, 3, 3, 3, 3, 3,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+        3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 3, 3, 1, 1, 1, 1, 1,
+        1, 1, 1, 1, 3, 3, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 3, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 3,
+        3, 1, 1, 1, 1, 1, 2, 1, 1, 1, 3, 3, 3, 1, 2, 2, 1, 1, 1, 1, 1, 1, 3, 3, 3, 1, 2, 2, 1, 1,
+        1, 1, 1, 1, 3, 3, 3, 1, 1, 1, 2, 1, 1, 1, 1, 1, 3, 3, 3, 1, 1, 1, 1, 2, 1, 1, 1, 1, 3, 3,
+        3, 1, 1, 1, 1, 2, 1, 1, 1, 1, 3, 3, 3, 3, 3, 1, 1, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 1, 2,
+        3, 3, 3, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     ];
     let map = Map::new(12, 12, 64, tiles);
-    let max_x : i32 = (map.cols * 64 - 512)-1;
-    let max_y : i32 = (map.cols * 64 - 512)-1;
+    let max_x: i32 = (map.cols * 64 - 512) - 1;
+    let max_y: i32 = (map.cols * 64 - 512) - 1;
     let mut camera = Camera::new(512, 512, max_x, max_y);
-    let mut prev_tick : u64 = 0;
+    let mut prev_tick: u64 = 0;
     'main: loop {
         let start = SystemTime::now();
         let se = start.duration_since(UNIX_EPOCH).expect("error on time");
         let tick = se.as_secs() * 1000 + se.subsec_nanos() as u64 / 1_000_000;
-        let mut delta : f64 = (tick as f64 - prev_tick as f64) / 1000.0;
+        let mut delta: f64 = (tick as f64 - prev_tick as f64) / 1000.0;
         prev_tick = tick;
         delta = fmin(0.75, delta);
         for _event in e.poll_iter() {
@@ -70,16 +71,20 @@ fn main() {
         can.clear();
         map.draw_map(&texture, &camera, &mut can);
         can.present();
-        let keys : HashSet<_> = e.keyboard_state().pressed_scancodes().filter_map(Keycode::from_scancode).collect();
-        let mut move_x : i32 = 0;
-        let mut move_y : i32 = 0;
+        let keys: HashSet<_> = e
+            .keyboard_state()
+            .pressed_scancodes()
+            .filter_map(Keycode::from_scancode)
+            .collect();
+        let mut move_x: i32 = 0;
+        let mut move_y: i32 = 0;
         for i in &keys {
             match *i {
                 Keycode::Left => {
                     move_x = -1;
                 }
                 Keycode::Right => {
-                     move_x = 1;
+                    move_x = 1;
                 }
                 Keycode::Up => {
                     move_y = -1;
@@ -87,12 +92,12 @@ fn main() {
                 Keycode::Down => {
                     move_y = 1;
                 }
-                    _ => {}
-                }
+                _ => {}
             }
-            if move_x != 0 || move_y != 0 {
-                camera.move_camera(delta, move_x, move_y);
-                println!("x: {} y: {} delta: {}", move_x, move_y, delta);
-            }
-   }
+        }
+        if move_x != 0 || move_y != 0 {
+            camera.move_camera(delta, move_x, move_y);
+            println!("x: {} y: {} delta: {}", move_x, move_y, delta);
+        }
+    }
 }
--- source/library/map/src/lib.rs
+++ source/library/map/src/lib.rs
@@ -1,36 +1,34 @@
-
-
 pub mod level {
 
+    use sdl2::rect::Rect;
     use std::cmp::max;
     use std::cmp::min;
-    use sdl2::rect::Rect;
 
     pub struct Camera {
-       pub  x: i32,
-       pub y: i32,
-       pub width: i32,
-       pub height: i32,
-       pub max_x: i32,
-       pub max_y: i32,
-       pub speed: i32
+        pub x: i32,
+        pub y: i32,
+        pub width: i32,
+        pub height: i32,
+        pub max_x: i32,
+        pub max_y: i32,
+        pub speed: i32,
     }
-    
+
     impl Camera {
         pub fn new(w: i32, h: i32, mx: i32, my: i32) -> Camera {
-            Camera { 
-                x:0,
-                y:0, 
-                width:w,
-                height:h, 
-                max_x: mx, 
+            Camera {
+                x: 0,
+                y: 0,
+                width: w,
+                height: h,
+                max_x: mx,
                 max_y: my,
-                speed: 512
+                speed: 512,
             }
         }
         pub fn move_camera(&mut self, delta: f64, dx: i32, dy: i32) {
-            let dx_val : f64 = dx as f64 * self.speed as f64 * delta;
-            let dy_val : f64 = dy as f64 * self.speed as f64 * delta;
+            let dx_val: f64 = dx as f64 * self.speed as f64 * delta;
+            let dy_val: f64 = dy as f64 * self.speed as f64 * delta;
             self.x += dx_val as i32;
             self.y += dy_val as i32;
             self.x = max(0, min(self.x, self.max_x));
@@ -42,36 +40,56 @@ pub mod level {
         pub cols: i32,
         pub rows: i32,
         pub tsize: i32,
-        pub layers: Vec<u8>
+        pub layers: Vec<u8>,
     }
-    
+
     impl Map {
         pub fn new(col: i32, row: i32, ts: i32, l: Vec<u8>) -> Map {
             Map {
                 cols: col,
                 rows: row,
                 tsize: ts,
-                layers: l
+                layers: l,
             }
         }
-    
-        pub fn draw_map(&self, texture: &sdl2::render::Texture, camera: &Camera, can: &mut sdl2::render::Canvas<sdl2::video::Window>) {
-            let start_col =   camera.x / self.tsize as i32;
-            let end_col    =  start_col + (camera.width / self.tsize as i32);
-            let start_row  =  camera.y / self.tsize as i32;
-            let end_row    =  start_row +  (camera.height / self.tsize as i32);
-            let cx : i32 = camera.x as i32;
-            let cy : i32 = camera.y as i32;
-            let off_x : i32 = -cx + start_col as i32 * self.tsize as i32;
-            let off_y : i32 = -cy + start_row as i32 * self.tsize as i32;
-            for i in start_col ..= end_col {
-                for z in start_row ..= end_row {
-                    let tile : u8 = self.layers[(z * self.rows + i) as usize];
-                    let x : i32 = (i- start_col) * self.tsize + off_x;
-                    let y : i32 = (z- start_row) * self.tsize + off_y;
-                    let t : i32 = tile as i32;
+
+        pub fn draw_map(
+            &self,
+            texture: &sdl2::render::Texture,
+            camera: &Camera,
+            can: &mut sdl2::render::Canvas<sdl2::video::Window>,
+        ) {
+            let start_col = camera.x / self.tsize as i32;
+            let end_col = start_col + (camera.width / self.tsize as i32);
+            let start_row = camera.y / self.tsize as i32;
+            let end_row = start_row + (camera.height / self.tsize as i32);
+            let cx: i32 = camera.x as i32;
+            let cy: i32 = camera.y as i32;
+            let off_x: i32 = -cx + start_col as i32 * self.tsize as i32;
+            let off_y: i32 = -cy + start_row as i32 * self.tsize as i32;
+            for i in start_col..=end_col {
+                for z in start_row..=end_row {
+                    let tile: u8 = self.layers[(z * self.rows + i) as usize];
+                    let x: i32 = (i - start_col) * self.tsize + off_x;
+                    let y: i32 = (z - start_row) * self.tsize + off_y;
+                    let t: i32 = tile as i32;
                     if t != 0 {
-                        can.copy(&texture, Some(Rect::new((t-1) as i32 * self.tsize as i32, 0, self.tsize as u32, self.tsize as u32)), Some(Rect::new(x as i32, y as i32, self.tsize as u32, self.tsize as u32))).expect("on copy");
+                        can.copy(
+                            texture,
+                            Some(Rect::new(
+                                (t - 1) as i32 * self.tsize as i32,
+                                0,
+                                self.tsize as u32,
+                                self.tsize as u32,
+                            )),
+                            Some(Rect::new(
+                                x as i32,
+                                y as i32,
+                                self.tsize as u32,
+                                self.tsize as u32,
+                            )),
+                        )
+                        .expect("on copy");
                     }
                 }
             }
