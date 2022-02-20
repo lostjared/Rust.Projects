07af49e52eeed12d7deec4b7017bc3f2d1ef96cc
Jared Bruni <jaredbruni@gmail.com>
2022-02-20 07:20:27 -0800

    print out array size

--- source/text2c_stdin/src/main.rs
+++ source/text2c_stdin/src/main.rs
@@ -5,10 +5,11 @@ fn print_data() -> io::Result<()> {
     let mut buffer = Vec::new();
     io::stdin().read_to_end(&mut buffer)?;
     println!("unsigned char bytes[] = {{");
-    for i in buffer {
+    for i in &buffer {
         print!("{:#04x},", i);
     }
     println!("0x0}};");
+    println!("unsigned long len = {:#04x};", buffer.len());
     Ok(())
 }
 
