71ee606c4bca13ea95926d320670479a049aabc2
Jared Bruni <jaredbruni@gmail.com>
2022-02-20 07:12:51 -0800

    added new test of reading from stdin

---
+++ source/text2c_stdin/src/main.rs
@@ -0,0 +1,18 @@
+use std::io;
+use std::io::prelude::*;
+
+fn print_data() -> io::Result<()> {
+    let mut buffer = Vec::new();
+    io::stdin().read_to_end(&mut buffer)?;
+    println!("unsigned char bytes[] = {{");
+    for i in buffer {
+        print!("{:#04x},", i);
+    }
+    println!("0x0}};");
+    Ok(())
+}
+
+fn main() -> io::Result<()> {
+    print_data()?;
+    Ok(())
+}
