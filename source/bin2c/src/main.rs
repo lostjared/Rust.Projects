80b4c5f9645a9724c5e75b385189ecb6cc216f95
Jared Bruni <jaredbruni@gmail.com>
2022-02-20 07:24:17 -0800

    added read from stdin when no args provided

--- source/bin2c/src/main.rs
+++ source/bin2c/src/main.rs
@@ -26,6 +26,28 @@ fn bin2c(infile: &str) -> std::io::Result<()> {
     Ok(())
 }
 
+fn bin2c_stdin() -> std::io::Result<()> {
+    println!("unsigned char bytes[] = {{");
+    let mut len : usize = 0;
+    loop {
+        let mut buf : [u8; 256] = [0; 256];
+        let val = std::io::stdin().read(&mut buf).expect("on read");
+
+        for i in 0..val {
+            print!("{:#04x},", buf[i]);
+        }
+
+        len += val as usize;
+
+        if val == 0 {
+            break;
+        }
+    }
+    println!("0x0}};\n");
+    println!("unsigned long length = {:#04x};", len);
+    Ok(())
+}
+
 
 fn main() -> std::io::Result<()> {
     let args: Vec<String> = env::args().collect();
@@ -34,7 +56,7 @@ fn main() -> std::io::Result<()> {
         bin2c(val1)?;
     }
     else {
-        println!("Error: input_file");
+        bin2c_stdin()?;
     }
     Ok(())
 }
