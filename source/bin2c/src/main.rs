d285270f28461347c63ba3c7ef6683f662c376cb
Jared Bruni <jaredbruni@gmail.com>
2022-02-20 18:39:47 -0800

    formatting

--- source/bin2c/src/main.rs
+++ source/bin2c/src/main.rs
@@ -1,14 +1,13 @@
-
+use std::env;
 use std::fs::File;
 use std::io::Read;
-use std::env;
 
 fn bin2c(infile: &str) -> std::io::Result<()> {
     let mut f = File::open(infile).expect("could not open file");
     println!("unsigned char bytes[] = {{");
-    let mut len : usize = 0;
+    let mut len: usize = 0;
     loop {
-        let mut buf : [u8; 256] = [0; 256];
+        let mut buf: [u8; 256] = [0; 256];
         let val = f.read(&mut buf).expect("on read");
 
         for i in 0..val {
@@ -28,15 +27,13 @@ fn bin2c(infile: &str) -> std::io::Result<()> {
 
 fn bin2c_stdin() -> std::io::Result<()> {
     println!("unsigned char bytes[] = {{");
-    let mut len : usize = 0;
+    let mut len: usize = 0;
     loop {
-        let mut buf : [u8; 256] = [0; 256];
+        let mut buf: [u8; 256] = [0; 256];
         let val = std::io::stdin().read(&mut buf).expect("on read");
-
         for i in 0..val {
             print!("{:#04x},", buf[i]);
         }
-
         len += val as usize;
 
         if val == 0 {
@@ -48,14 +45,12 @@ fn bin2c_stdin() -> std::io::Result<()> {
     Ok(())
 }
 
-
 fn main() -> std::io::Result<()> {
     let args: Vec<String> = env::args().collect();
     if args.len() == 2 {
         let val1 = args.get(1).unwrap();
         bin2c(val1)?;
-    }
-    else {
+    } else {
         bin2c_stdin()?;
     }
     Ok(())
