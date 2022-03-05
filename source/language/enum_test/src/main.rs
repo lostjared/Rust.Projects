2e07afa48ade4583b230c939565a9364b4bc1511
Jared Bruni <jaredbruni@gmail.com>
2022-03-04 19:44:36 -0800

    enum test

---
+++ source/language/enum_test/src/main.rs
@@ -0,0 +1,28 @@
+
+
+enum ValueType {
+    TYPE1,
+    TYPE2,
+    TYPE3(String),
+}
+
+fn to_value(t: ValueType) {
+    match t {
+        ValueType::TYPE1 => {
+            println!("type 1");
+        }
+        ValueType::TYPE2 => {
+            println!("type 2");
+        }
+        ValueType::TYPE3(s) => {
+            println!("type 3 {}", s);
+        }
+    }
+}
+
+
+fn main() {
+    to_value(ValueType::TYPE1);
+    to_value(ValueType::TYPE2);
+    to_value(ValueType::TYPE3("test ".to_string()));
+}
