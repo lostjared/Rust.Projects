50126aa538e6ee6d5cd5c24ed3b8f75ce6da6202
Jared Bruni <jaredbruni@gmail.com>
2022-08-03 12:04:56 -0700

    quick practice

---
+++ source/guessing_game/src/main.rs
@@ -0,0 +1,32 @@
+
+use std::io;
+use rand::Rng;
+
+fn guess_number() -> u32 {
+    println!("Enter number: ");
+    let mut input_text : String = String::new();
+    io::stdin().read_line(&mut input_text).expect("error on readline");
+    input_text.pop();
+    let num = input_text.parse().unwrap();
+    num
+}
+
+fn main() {
+    loop {
+        println!("Guessing game guess between 1-10");
+        let value = guess_number();
+        let mut random = rand::thread_rng();
+        if value == random.gen_range(1..10) {
+            println!("Correct you win...\n");
+        } else {
+            println!("Incorrect you lose..\n");
+        }
+        println!("Try again (yes/no)");
+        let mut input_text : String = String::new();
+        io::stdin().read_line(&mut input_text).expect("error on readline");
+        input_text.pop();
+        if input_text != "yes" {
+            break;
+        }
+    }
+}
