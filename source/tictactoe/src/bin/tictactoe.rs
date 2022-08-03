3ce379d205793439ef0b89d0ee3fe01480bcc33b
Jared Bruni <jaredbruni@gmail.com>
2022-08-03 16:47:05 -0700

    working on practice

---
+++ source/tictactoe/src/bin/tictactoe.rs
@@ -0,0 +1,39 @@
+use tictactoe::tictactoe::Grid;
+
+fn get_move(player: u32) -> (usize, usize, u32) {
+    println!("Player {} Enter x y ", player);
+    let mut input_text : String = String::new();
+    std::io::stdin().read_line(&mut input_text).expect("on readline");
+    input_text.pop();
+    if !input_text.contains(" ") {
+        println!("Needs to contain two numbers");
+        return get_move(player);
+    }
+    let value : Vec<&str> = input_text.split(" ").collect();
+    let x : usize = value[0].parse().unwrap();
+    let y : usize = value[1].parse().unwrap();
+
+    if !(x <= 2 && y <= 2) {
+        println!("must be value 0-2");
+        return get_move(player);
+    }
+
+    (x, y, player)
+}
+
+fn main() {
+    let mut g : Grid = Grid::new();
+    g.print_board();
+    loop  {
+        let rt_val = get_move(g.turn);
+        g.go_turn(rt_val.0, rt_val.1, rt_val.2);
+        g.print_board();
+        if g.check_game_over() == -1 {
+            g.switch_turn();        
+        } else {
+            println!("Player {} Won!\n", g.check_game_over());
+            g.print_board();
+            break;
+        }          
+    }
+}
---
+++ source/tictactoe/src/lib.rs
@@ -0,0 +1,89 @@
+pub mod tictactoe {
+
+    pub struct Grid {
+        grid: Box<[[u32; 4]; 4]>,
+        pub turn: u32,
+    }
+
+    impl Grid {
+        pub fn new() -> Grid {
+            Grid {
+                grid: Box::new([[0; 4]; 4]),
+                turn: 1,
+            }
+        }
+
+        pub fn go_turn(&mut self, x: usize, y: usize, t: u32) {
+            self.grid[x][y] = t;
+        }
+
+        pub fn check_type(&self, t: u32) -> bool {
+            if self.grid[0][0] == t && self.grid[0][1] == t && self.grid[0][2] == t {
+                return true;
+            }
+            if self.grid[1][0] == t && self.grid[1][1] == t && self.grid[1][2] == t {
+                return true;
+            }
+            if self.grid[2][0] == t && self.grid[2][1] == t && self.grid[2][2] == t {
+                return true;
+            }
+
+            if self.grid[0][0] == t && self.grid[0][1] == t && self.grid[0][2] == t {
+                return true;
+            }
+
+            if self.grid[1][0] == t && self.grid[1][1] == t && self.grid[1][2] == t {
+                return true;
+            } 
+            if self.grid[2][0] == t && self.grid[2][1] == t && self.grid[2][2] == t {
+                return true;
+            }
+            if self.grid[0][0] == t && self.grid[1][1] == t && self.grid[2][2] == t {
+                return true;
+            }
+            if self.grid[0][2] == t && self.grid[1][1] == t && self.grid[2][0] == t {
+                return true;
+            }
+            false
+        }
+
+        pub fn check_game_over(&self) -> i32 {
+
+            if self.check_type(1) == true {
+                return 1;
+            }
+
+            if self.check_type(2) == true {
+                return 2;
+            }
+
+            -1
+        }
+
+        pub fn switch_turn(&mut self) {
+            if self.turn == 2 {
+                self.turn = 1;
+            } else {
+                self.turn = 2;
+            }
+        }
+
+        pub fn print_board(&self) {
+            for z in 0..3 {
+                print!("|");
+                for i in 0..3 {
+                    if self.grid[i][z] == 1 {
+                        print!("X");
+                    }
+                    else if self.grid[i][z] == 2 {
+                       print!("O");
+                    }
+                    else {
+                        print!("_");
+                    }
+                }
+                println!("|");
+            }
+        }
+    }
+}
