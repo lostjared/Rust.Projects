ba09c5ed98deb98cd6484ca3e6281dc489bbda27
Jared Bruni <jaredbruni@gmail.com>
2022-08-04 14:20:19 -0700

    draw grid

--- source/tictactoe/src/bin/tictactoe-sdl.rs
+++ source/tictactoe/src/bin/tictactoe-sdl.rs
@@ -1,8 +1,7 @@
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
-use sdl2::pixels::PixelFormatEnum;
 use sdl2::rect::Rect;
-
+use sdl2::pixels::Color;
 fn main() {
     let width = 1280;
     let height = 720;
@@ -22,7 +21,14 @@ fn main() {
                 _ => {}
             }
         }
+        can.set_draw_color(Color::RGB(0, 0, 0));
         can.clear();
+
+        can.set_draw_color(Color::RGB(255, 255, 255));
+        can.fill_rect(Some(Rect::new(100, 200, 1280-200, 25))).expect("draw rect");
+        can.fill_rect(Some(Rect::new(100, 400, 1280-200, 25))).expect("draw rect");
+        can.fill_rect(Some(Rect::new(1280/2-200, 25, 25, 720-100))).expect("draw rect");
+        can.fill_rect(Some(Rect::new(1280/2+200, 25, 25, 720-100))).expect("draw rect");
         can.present();
     }
 }
