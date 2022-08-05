57a0af3262bf6886b31a59fdbf4aaa99a0090174
Jared Bruni <jaredbruni@gmail.com>
2022-08-05 14:58:07 -0700

    added practice code

---
+++ source/in2rs/src/main.rs
@@ -0,0 +1,37 @@
+use std::io::prelude::*;
+
+fn slash_seq(input: &String) -> String {
+    let mut value : String = String::new();
+    for i in input.chars() {
+        if i == '\\' {
+            value.push_str("\\\\");
+        }
+        else {
+            value.push(i);
+        }
+    }
+    value
+}
+
+fn convert_to_rs() -> String {
+    let mut value : String = String::new();
+    value.push_str("let v = vec![");
+    loop {
+        let mut input_text : String = String::new();
+        let val = std::io::stdin().read_line(&mut input_text).expect("on read");
+        input_text.pop();
+        value.push_str(&format!("\n\"{}\"", &slash_seq(&input_text)));
+        if val == 0  {
+            break;
+        } else {
+            value.push_str(",");
+        }
+    }
+    value.push_str("];\n");
+    value
+}
+
+fn main() {
+    let s : String = convert_to_rs();
+    println!("{}", s);
+}
