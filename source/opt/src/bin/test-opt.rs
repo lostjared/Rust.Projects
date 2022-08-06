c9e3660d0f8a3eb62b5559512ca8ca72a81f567e
Jared Bruni <jaredbruni@gmail.com>
2022-08-06 13:58:47 -0700

    Updated added match

--- source/opt/src/bin/test-opt.rs
+++ source/opt/src/bin/test-opt.rs
@@ -3,12 +3,24 @@ use opt::argz;
 
 fn main() {
     let args = std::env::args().collect();
-    argz::optarg(&args,"t:x", |i: char, param: String| {
+    argz::optarg(&args,"i:o:a", |i: char, param: String| {
         if param.len() > 0 {
             println!("ch: {} param: {} ", i, param);
         } else {
             println!("chars: {}", i);
         }
-    });
+        match i {
+            'i' => {
+                println!("Input argument: {}", param);
+            }
+            'o' => {
+                println!("Output argument: {}", param);
 
+            }
+            'a' => {
+                println!("A argument\n");
+            }
+            _ => { println!("unknown argument "); }
+        }
+    });
 }
