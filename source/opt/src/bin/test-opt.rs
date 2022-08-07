0035bc2b75eea3009391d86046b5177fedc7b4ef
Jared Bruni <jaredbruni@gmail.com>
2022-08-07 11:14:33 -0700

    Updated test

--- source/opt/src/bin/test-opt.rs
+++ source/opt/src/bin/test-opt.rs
@@ -2,25 +2,18 @@ use opt::argz;
 
 fn main() {
     let args = std::env::args().collect();
-    argz::getopt(&args, "i:o:a", |i: char, param: String| {
-        if !param.is_empty() {
-            println!("ch: {} param: {} ", i, param);
-        } else {
-            println!("chars: {}", i);
+    argz::getopt(&args, "i:o:a", |i: char, param: String| match i {
+        'i' => {
+            println!("Input argument: {}", param);
         }
-        match i {
-            'i' => {
-                println!("Input argument: {}", param);
-            }
-            'o' => {
-                println!("Output argument: {}", param);
-            }
-            'a' => {
-                println!("A argument\n");
-            }
-            _ => {
-                println!("unknown argument ");
-            }
+        'o' => {
+            println!("Output argument: {}", param);
+        }
+        'a' => {
+            println!("A argument\n");
+        }
+        _ => {
+            println!("unknown argument ");
         }
     });
 }
