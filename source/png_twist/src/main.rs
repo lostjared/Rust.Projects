7d1002334c3e31690f529c7d2429d7f4624dadee
Jared Bruni <jaredbruni@gmail.com>
2022-08-09 12:08:31 -0700

    formatting

--- source/png_twist/src/main.rs
+++ source/png_twist/src/main.rs
@@ -1,16 +1,14 @@
 use image;
-use image::GenericImageView; 
-use image::Pixel;
+use image::GenericImageView;
 use image::ImageBuffer;
+use image::Pixel;
 
-fn read_bytes_and_proc(input: &str, output_file: &str, id: u8) {
+fn process_image(input: &str, output_file: &str, id: u8) {
     let img = image::open(input).expect("File not found!");
     let (w, h) = img.dimensions();
     let mut output = ImageBuffer::new(w, h);
     for (x, y, pixel) in img.pixels() {
-        output.put_pixel(x, y, 
-            pixel.map(|p| p.wrapping_mul(id).wrapping_add(p^15))
-        );
+        output.put_pixel(x, y, pixel.map(|p| p.wrapping_mul(id).wrapping_add(p ^ 15)));
     }
     output.save(output_file).expect("on save");
     println!("Wrote to file: {}", output_file);
@@ -20,7 +18,7 @@ fn main() {
     let args: Vec<String> = std::env::args().collect();
     if args.len() == 4 {
         let index = args.get(3).unwrap().parse().unwrap();
-        read_bytes_and_proc(args.get(1).unwrap(), args.get(2).unwrap(), index);
+        process_image(args.get(1).unwrap(), args.get(2).unwrap(), index);
     } else {
         println!("{}: input.png output.png level_index", args.get(0).unwrap());
     }
