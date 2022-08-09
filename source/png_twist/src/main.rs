232c40ff4e9c4040372d2948ab792f8f96677b0a
Jared Bruni <jaredbruni@gmail.com>
2022-08-09 11:40:26 -0700

    updated

--- source/png_twist/src/main.rs
+++ source/png_twist/src/main.rs
@@ -3,17 +3,17 @@ use image::GenericImageView;
 use image::Pixel;
 use image::ImageBuffer;
 
-fn read_bytes_and_proc(input: &str, output_file: &str, id: u32) {
+fn read_bytes_and_proc(input: &str, output_file: &str, id: u8) {
     let img = image::open(input).expect("File not found!");
     let (w, h) = img.dimensions();
     let mut output = ImageBuffer::new(w, h);
-    let value = 20;
     for (x, y, pixel) in img.pixels() {
         output.put_pixel(x, y, 
-            pixel.map(|p| p.wrapping_add(p^value))
+            pixel.map(|p| p.wrapping_add(p^id))
         );
     }
     output.save(output_file).expect("on save");
+    println!("Wrote to file: {}", output_file);
 }
 
 fn main() {
