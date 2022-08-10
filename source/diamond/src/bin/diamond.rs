1a6b318afe5edfd80bf6ffc0bd7ffaf7b48c607b
Jared Bruni <jaredbruni@gmail.com>
2022-08-10 14:25:21 -0700

    added formatting

--- source/diamond/src/bin/diamond.rs
+++ source/diamond/src/bin/diamond.rs
@@ -1,5 +1,5 @@
 use diamond::dmd;
-
+/// main function example
 fn main() {
     let args: Vec<String> = std::env::args().skip(1).collect();
     let d = dmd::Diamond::new(&args);
--- source/diamond/src/lib.rs
+++ source/diamond/src/lib.rs
@@ -1,9 +1,10 @@
-// like perls <> operator
+//!like perls <> operator
 pub mod dmd {
 
     use std::io::BufRead;
     use std::fmt;
 
+    // Diamond structure
     pub struct Diamond {
         files: Vec<String>,
         cur_index: usize,
@@ -11,6 +12,7 @@ pub mod dmd {
     }
 
     impl Diamond {
+        /// create new diamond with list of files to read
         pub fn new(args: &Vec<String>) -> Self {
             Diamond {
                 files: args.to_owned(),
@@ -20,7 +22,7 @@ pub mod dmd {
                 ),
             }
         }
-
+        /// read next line from list of files one after the other
         pub fn read_next(&mut self) -> Option<String> {
             let mut line: String = String::new();
             let file_len = self.file_open.read_line(&mut line).expect("on read");
@@ -40,12 +42,14 @@ pub mod dmd {
 
     impl Iterator for Diamond {
         type Item = String;
+        /// next item for iterator
         fn next(&mut self) -> Option<Self::Item> {
             self.read_next()
         }
     }
 
     impl fmt::Display for Diamond {
+        /// format display for formater
         fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
             write!(f, "{:?} len: {}", self.files, self.files.len())
         }
