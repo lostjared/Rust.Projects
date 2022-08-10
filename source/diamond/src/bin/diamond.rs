24b7999b28b8f41b6040ac3ad550c6bb14b0a939
Jared Bruni <jaredbruni@gmail.com>
2022-08-10 10:52:06 -0700

    removed comment

--- source/diamond/src/bin/diamond.rs
+++ source/diamond/src/bin/diamond.rs
@@ -3,15 +3,6 @@ use diamond::dmd;
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     let d = dmd::Diamond::new(&args);
-    /*
-    loop {
-        let line = d.read_next();
-        if line == None {
-            break;
-        }
-        println!("{}", line.unwrap());
-    } */
-    // or
     for i in d {
         println!("{}", i);
     }
