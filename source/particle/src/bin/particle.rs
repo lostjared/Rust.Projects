78e25dd3de559e41ce450396b8b202ebd2419d8c
Jared Bruni <jaredbruni@gmail.com>
2022-08-17 14:17:08 -0700

    new practice project

---
+++ source/particle/src/bin/particle.rs
@@ -0,0 +1,69 @@
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
+use sdl2::rect::Rect;
+use std::time::{SystemTime, UNIX_EPOCH};
+
+use particle::particle_emiter::Emiter;
+
+/// main function - entry point
+fn main() {
+    let width = 1280;
+    let height = 720;
+    let sdl = sdl2::init().unwrap();
+    let video = sdl.video().unwrap();
+    let window = video
+        .window("Particle", width, height)
+        .resizable()
+        .opengl()
+        .build()
+        .unwrap();
+    let mut can = window
+        .into_canvas()
+        .build()
+        .map_err(|e| e.to_string())
+        .expect("Error on canvas");
+    let mut e = sdl.event_pump().unwrap();
+    let mut prev_tick: u64 = 0;
+    let mut tick_count = 0;
+    let mut emiter  = Emiter::new();
+    emiter.init();
+    'main: loop {
+        for _event in e.poll_iter() {
+            match _event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'main,
+                _ => {}
+            }
+        }
+        can.set_draw_color(Color::RGB(0, 0, 0));
+        can.clear();
+
+        for i in &emiter.part {
+            can.set_draw_color(Color::RGB(i.depth, i.depth, i.depth));
+            can.fill_rect(Some(Rect::new(
+                i.x,
+                i.y,
+                8,
+                8,
+            )))
+            .expect("on fill");
+        }
+
+
+        can.present();
+        let start = SystemTime::now();
+        let se = start.duration_since(UNIX_EPOCH).expect("error on time");
+        let tick = se.as_secs() * 1000 + se.subsec_nanos() as u64 / 1_000_000;
+        let ptick = tick - prev_tick;
+        prev_tick = tick;
+        tick_count += ptick;
+        if tick_count > 75 {
+            tick_count = 0;
+            emiter.update();            
+        }
+    }
+}
---
+++ source/particle/src/lib.rs
@@ -0,0 +1,66 @@
+
+
+pub mod particle_emiter {
+
+    use rand::Rng;
+
+    pub const NUM_PARTICLES: usize = 1024;
+    
+    #[derive(Copy,Clone)]
+    pub struct Particle {
+        pub x: i32,
+        pub y: i32,
+        pub depth: u8,
+    }
+
+    pub struct Emiter {
+        pub part: [Particle; NUM_PARTICLES],
+    }
+    
+    impl Particle {
+        pub fn new() -> Self {
+            let mut rng = rand::thread_rng();
+            let rand_x = rng.gen_range(0..1280-32);
+            let rand_y = rng.gen_range(0..720-32);    
+            let rand_depth = rng.gen_range(0..255-16);
+            Particle {
+                x: rand_x,
+                y: rand_y,
+                depth: rand_depth
+            }
+        }
+        pub fn move_down(&mut self) {
+            let mut rng = rand::thread_rng();
+            self.y += 16;
+            self.depth += 16;
+            if self.depth >= 0xFE-16 {
+                self.depth = 1;
+            }
+            if self.y > 720-8 {
+                self.y = rng.gen_range(0..720-32);
+                self.x = rng.gen_range(0..1280-32);
+            }
+        }
+    }
+
+    impl Emiter {
+        pub fn new() -> Self {
+            Emiter {
+                part: [Particle { x: 0, y: 0, depth: 0 }; NUM_PARTICLES]
+            }
+        }
+
+        pub fn init(&mut self) {
+            for i in 0..NUM_PARTICLES {
+                self.part[i] = Particle::new();
+            }
+        }
+
+        pub fn update(&mut self) {
+            for i in &mut self.part {
+                i.move_down();
+            }
+        }
+    }
+
+}
