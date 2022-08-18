5682051c2ec78a4b6ab0a9d5894abee62595b122
Jared Bruni <jaredbruni@gmail.com>
2022-08-18 10:47:48 -0700

    added trait example

---
+++ source/trait_ex/src/main.rs
@@ -0,0 +1,54 @@
+// polymorphism with traits
+
+struct Monkey {}
+struct Cat {}
+struct Human {}
+
+trait Animal {
+    fn eat(&self);
+    fn die(&self);
+}
+
+impl Animal for Monkey {
+    fn eat(&self) {
+        println!("Monkey eats something");    
+    }
+
+    fn die(&self) {
+        println!("Monkey dies");
+    }
+}
+
+impl Animal for Cat {
+    fn eat(&self) {
+        println!("Cat eats something");    
+    }
+
+    fn die(&self) {
+        println!("Cat dies");
+    }
+}
+
+impl Animal for Human {
+    fn eat(&self) {
+        println!("Human eats something");    
+    }
+
+    fn die(&self) {
+        println!("Human dies");
+    }
+}
+
+fn eat_and_die(o: &dyn Animal) {
+    o.eat();
+    o.die();
+}
+
+fn main() {
+    let human = Human{};
+    let monkey = Monkey{};
+    let cat = Cat{};
+    eat_and_die(&human);
+    eat_and_die(&monkey);
+    eat_and_die(&cat);
+}
