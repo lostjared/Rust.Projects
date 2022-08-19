eced78c09cce6c31b57232cc9690dae34db63bbd
Jared Bruni <jaredbruni@gmail.com>
2022-08-19 10:49:07 -0700

    ex03

---
+++ source/thread_test/channel/channel_ex03/src/main.rs
@@ -0,0 +1,27 @@
+#[macro_use]
+extern crate crossbeam;
+
+use crossbeam::channel::{unbounded};
+use std::thread;
+
+#[derive(Debug)]
+enum Message {
+    Message1,
+    Message2,
+}
+
+fn main() {
+    let (channel_send, channel_recv) = unbounded();
+    thread::spawn(move || {
+        for _ in 0..5 {
+            channel_send.send(Message::Message1).unwrap();
+        }
+        channel_send.send(Message::Message2).unwrap()
+    });
+
+    for _ in 0..6 {
+        select! { 
+            recv(channel_recv) -> msg => println!("{:?}", msg),
+        }
+    }
+}
