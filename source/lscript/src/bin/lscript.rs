7b186760556e5d769867e7f2deb2078ce0da87c5
Jared Bruni <jaredbruni@gmail.com>
2022-08-20 12:14:47 -0700

    updated

--- source/lscript/src/bin/lscript.rs
+++ source/lscript/src/bin/lscript.rs
@@ -4,8 +4,11 @@ use sdl2::pixels::Color;
 use sdl2::rect::Rect;
 use std::time::{SystemTime, UNIX_EPOCH};
 
+use lscript::scr::MovementObject;
+
 /// main function - entry point
 fn main() {
+    let args : Vec<String> = std::env::args().collect();
     let width = 1280;
     let height = 720;
     let sdl = sdl2::init().unwrap();
@@ -24,6 +27,9 @@ fn main() {
     let mut e = sdl.event_pump().unwrap();
     let mut prev_tick: u64 = 0;
     let mut tick_count = 0;
+
+    let movement = MovementObject::load_from_file(args.get(1).unwrap());
+    movement.print_movement();
 
     'main: loop {
         for _event in e.poll_iter() {
--- source/lscript/src/lib.rs
+++ source/lscript/src/lib.rs
@@ -1,8 +1,61 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+
+
+pub mod scr {
+
+ 
+    #[derive(PartialEq,Copy,Clone,Debug)]
+    enum Direction {
+        Left,
+        Right,
+        Up,
+        Down
     }
+
+    #[derive(Copy, Clone)]
+    struct Movement {
+        direction: Direction,
+        steps: i32
+    }
+
+    pub struct MovementObject {
+        lst: Vec<Movement>,
+    }
+
+    impl MovementObject {
+
+        pub fn load_from_file(filename: &str) -> Self {
+            let mut lst : Vec<Movement> = vec![];
+            let contents = std::fs::read_to_string(filename).expect("Error reading the file");
+            for i in contents.lines() {
+                let pos = i.find(':');
+                if pos == None {
+                    continue;
+                }
+                let pos = pos.unwrap();
+                let left = &i[0..pos];
+                let right = &i[pos+1..];
+                println!("{} right: {} ", left, right);
+                let ch = left.chars().nth(0).unwrap();
+                let dir : Direction = match ch {
+                    'L' => Direction::Left,
+                    'R' => Direction::Right,
+                    'U' => Direction::Up,
+                    'D' => Direction::Down,
+                    _ => { panic!("invalid type"); }
+                };
+                let l : Movement = Movement { direction: dir, steps: right.parse().unwrap() };
+                lst.push(l);
+            }
+            MovementObject {
+                lst: lst
+            }
+        }
+
+        pub fn print_movement(&self) {
+            for i in &self.lst {
+                println!("Move: {:?}, Steps: {}", i.direction, i.steps);
+            }
+        }
+    }
+
 }
