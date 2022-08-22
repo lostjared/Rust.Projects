5246a845b18199ad80c3e09cd03a47a5f518d7d7
Jared Bruni <jaredbruni@gmail.com>
2022-08-22 11:58:42 -0700

    formatting

--- source/bin2rs/src/main.rs
+++ source/bin2rs/src/main.rs
@@ -1,23 +1,23 @@
-fn convert_to_rs<T: std::io::BufRead + Sized>(mut reader: T) -> String {
+fn convert_to_rs<T>(mut reader: T) -> String
+where
+    T: std::io::BufRead + Sized,
+{
     let mut value: String = String::new();
     value.push_str("let v = vec![");
     let mut bytes = [0; 1024];
     loop {
-        let val = reader
-            .read(&mut bytes)
-            .expect("on read");
-            if val == 0 {
-                break;
-            }
-            for i in 0..val {
-                let s = format!("{:#04x},", bytes[i]);
-                value.push_str(&s);
-            }
+        let val = reader.read(&mut bytes).expect("on read");
+        if val == 0 {
+            break;
         }
+        for i in 0..val {
+            let s = format!("{:#04x},", bytes[i]);
+            value.push_str(&s);
+        }
+    }
     value.push_str("0x0];\n");
     value
 }
-
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
