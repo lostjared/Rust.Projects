190b8e792e46e35f3225408d4707e3f6ac57ecb0
Jared Bruni <jaredbruni@gmail.com>
2022-08-22 11:42:48 -0700

    added opt crate

--- source/index_files/src/bin/indexer.rs
+++ source/index_files/src/bin/indexer.rs
@@ -1,11 +1,30 @@
 
 use index_files::indexer::index_copy;
+use opt::argz;
 
 fn main() {
     let args : Vec<String> = std::env::args().collect();
-    if args.len() <= 2 || args.len() > 3 {
-        println!("use:\n\t{}: input_list.txt output_dir", args.get(0).unwrap());
+    let mut input_file = String::new();
+    let mut output_file= String::new();
+    let mut name_file = String::new();
+
+    let _rt_val = argz::getopt(&args, "i:o:n:", |i: char, param: String| match i {
+        'i' => {
+            input_file = param.to_owned();
+        }
+        'o' => {
+            output_file = param.to_owned();
+        }
+        'n' => {
+            name_file = param.to_owned();
+        }
+        _ => {
+        }
+    });
+    if input_file.is_empty() || output_file.is_empty() {
+        println!("use:\n\t -i input file\n\t -o output directory\n\t -n optional prefix name\n");
+        println!("{}:{}:{}", input_file, output_file, name_file);
         return;
     }
-    index_copy(args.get(1).unwrap(), args.get(2).unwrap());
+    index_copy(&input_file, &output_file, &name_file);
 }
--- source/index_files/src/lib.rs
+++ source/index_files/src/lib.rs
@@ -1,7 +1,7 @@
 
 pub mod indexer {
     pub use std::io::BufRead;
-    pub fn index_copy(filename: &str, output_dir: &str) {
+    pub fn index_copy(filename: &str, output_dir: &str, new_name: &str) {
         let f = std::fs::File::open(filename).expect("on opening of file");
         let mut reader = std::io::BufReader::new(f);
         let mut files : Vec<String> = Vec::new();
@@ -14,14 +14,22 @@ pub mod indexer {
             files.push(String::from(s.trim()));
         }
         let mut num = 0;
+        let sval = format!("{}", files.len()+1);
+        let count = sval.len();
         for i in &files {
             let in_file = std::path::Path::new(i);
             let fname = in_file.file_stem().unwrap();
             let ext = in_file.extension().unwrap();
-            let final_name = format!("{}/{}.{:08}.{}", output_dir, fname.to_string_lossy(), num, ext.to_string_lossy());
+            let final_name;
+            if new_name.is_empty() {
+                final_name = format!("{}/{}.{:0width$}.{}", output_dir, fname.to_string_lossy(), num, ext.to_string_lossy(),width=count);
+            } else {
+                final_name = format!("{}/{}.{:0width$}.{}", output_dir, new_name, num, ext.to_string_lossy(),width=count);
+            }
             let final_path = std::path::Path::new(&final_name);
             let final_s = final_path.to_string_lossy();
             std::fs::copy(i.to_string(), final_s.to_string()).expect("failed to rename");
+            println!("{} -> {}", i, final_s);
             num += 1;
         }
    }
