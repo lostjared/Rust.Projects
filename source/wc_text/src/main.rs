20808f204ebb154b1c9ee6b259a8bb3bd12b0298
Jared Bruni <jaredbruni@gmail.com>
2022-08-25 15:02:49 -0700

    used generic

--- source/wc_text/src/main.rs
+++ source/wc_text/src/main.rs
@@ -1,47 +1,44 @@
-
 use clap::{App, Arg};
-use std::io::{BufRead,Read};
 
 struct Arguments {
     filenames: Vec<String>,
     lines: bool,
     ch: bool,
-    words: bool
+    words: bool,
 }
 
 fn parse_args() -> Arguments {
-
     let matches = App::new("wc_text")
-    .version("0.1.0")
-    .author("Jared Bruni")
-    .help("count lines/bytes")
-    .arg(
-        Arg::with_name("lines")
-            .short('l')
-            .long("lines")
-            .help("print number of lines")
-    )
-    .arg(
-        Arg::with_name("bytes")
-            .short('b')
-            .long("bytes")
-            .help("output number of bytes")
-    )
-    .arg(
-        Arg::with_name("words")
-            .short('w')
-            .long("words")
-            .help("output number of words")
-    )
-    .arg(
-        Arg::with_name("files")
-            .value_name("FILE")
-            .help("input file")
-            .multiple(true)
-            .required(true)
-            .allow_invalid_utf8(true),
-    )
-    .get_matches();
+        .version("0.1.0")
+        .author("Jared Bruni")
+        .help("count lines/bytes")
+        .arg(
+            Arg::with_name("lines")
+                .short('l')
+                .long("lines")
+                .help("print number of lines"),
+        )
+        .arg(
+            Arg::with_name("bytes")
+                .short('b')
+                .long("bytes")
+                .help("output number of bytes"),
+        )
+        .arg(
+            Arg::with_name("words")
+                .short('w')
+                .long("words")
+                .help("output number of words"),
+        )
+        .arg(
+            Arg::with_name("files")
+                .value_name("FILE")
+                .help("input file")
+                .multiple(true)
+                .required(true)
+                .allow_invalid_utf8(true),
+        )
+        .get_matches();
     let f = matches.values_of_lossy("files").unwrap();
     let l = matches.is_present("lines");
     let b = matches.is_present("bytes");
@@ -50,21 +47,22 @@ fn parse_args() -> Arguments {
         filenames: f,
         lines: l,
         ch: b,
-        words: w,        
+        words: w,
     }
 }
 
-
-fn count_lines(filename: &str) -> usize {
-    let f = std::fs::File::open(filename).expect("on open");
-    let r = std::io::BufReader::new(f);
+fn count_lines<T>(r: T) -> usize
+where
+    T: std::io::BufRead + Sized,
+{
     let count = r.lines().count();
     count
 }
 
-fn count_chars(filename: &str) -> usize {
-    let f = std::fs::File::open(filename).expect("on open");
-    let mut r = std::io::BufReader::new(f);
+fn count_chars<T>(mut r: T) -> usize
+where
+    T: std::io::BufRead + Sized,
+{
     let mut s = String::new();
     r.read_to_string(&mut s).expect("on read");
     s.len()
@@ -77,41 +75,61 @@ fn remove_chars(input: &str) -> String {
             'a'..='z' | 'A'..='Z' | ' ' | '\n' => {
                 output.push(i);
             }
-            _ => {
-
-            }
+            _ => {}
         }
     }
     output
 }
 
-fn count_words(input: &str) -> usize {
-    let input = std::fs::read_to_string(input).expect("on read");
+fn count_words<T>(mut r: T) -> usize
+where
+    T: std::io::BufRead + Sized,
+{
+    let mut input = String::new();
+    let _len = r.read_to_string(&mut input).expect("on read");
     let val = remove_chars(&input);
     let values = val.split(" ");
     values.count()
 }
 
-
 fn count_text(args: &Arguments) -> std::io::Result<()> {
     for i in &args.filenames {
         if args.lines {
-            let c = count_lines(i);
+            let c;
+            if i == "." {
+                let r = std::io::stdin().lock();
+                c = count_lines(r);
+            } else {
+                let f = std::fs::File::open(i)?;
+                c = count_lines(std::io::BufReader::new(f));
+            }
             println!("\tlines: {}", c);
         }
         if args.ch {
-            let c = count_chars(i);
+            let c;
+            if i == "." {
+                let r = std::io::stdin().lock();
+                c = count_chars(r);
+            } else {
+                let f = std::fs::File::open(i)?;
+                c = count_chars(std::io::BufReader::new(f));
+            }
             println!("\tbytes: {}", c);
         }
         if args.words {
-            let c = count_words(i);
+            let c;
+            if i == "." {
+                let r = std::io::stdin().lock();
+                c = count_words(r);
+            } else {
+                let f = std::fs::File::open(i)?;
+                c = count_words(std::io::BufReader::new(f));
+            }
             println!("\twords; {}", c);
         }
     }
     Ok(())
 }
-
-
 
 fn main() -> std::io::Result<()> {
     let args = parse_args();
