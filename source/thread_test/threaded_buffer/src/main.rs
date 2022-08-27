455d84d95faad713d0a9557637605a0c5061e1dd
Jared Bruni <jaredbruni@gmail.com>
2022-08-27 16:09:32 -0700

    updated

--- source/thread_test/threaded_buffer/src/main.rs
+++ source/thread_test/threaded_buffer/src/main.rs
@@ -17,14 +17,27 @@ fn process_chunk(buf: &mut [u8], y: usize, width: usize, chunk: usize, bpp: usiz
     }
 }
 
+pub fn save_to_file(filename: &str, bytes: &[u8], width: usize, height: usize) {
+    let path = std::path::Path::new(filename);
+    let file = std::fs::File::create(path).unwrap();
+    let ref mut w = std::io::BufWriter::new(file);
+    let mut encoder = png::Encoder::new(w, width as u32, height as u32);
+    encoder.set_color(png::ColorType::Rgba);
+    encoder.set_depth(png::BitDepth::Eight);
+    let mut writer = encoder.write_header().unwrap();
+    let len = bytes.len();
+    writer.write_image_data(&bytes[0..len]).unwrap();
+}
+
 fn main() {
     let arguments: Vec<String> = std::env::args().collect();
     let filename = arguments.get(1).unwrap();
-    let mut im = FilterImage::load_from_png(filename);
+    let im = FilterImage::load_from_png(filename);
     let mut pos = 0;
     let num_threads = 8;
     let len = im.bytes.len();
-    let chunk = im.height / num_threads;
+    let height = im.height;
+    let chunk = height / num_threads;
     let width = im.width;
     let bpp = im.bpp;
     let mut handles = vec![];
@@ -42,7 +55,7 @@ fn main() {
     for j in handles {
         j.join().unwrap();
     }
-    im.bytes = values.lock().unwrap().clone();
-    im.save_to_file("output.png");
+    let bytes = values.lock().unwrap();
+    save_to_file("output.png", &bytes, width, height);
     println!("wrote to file: output.png");
 }
