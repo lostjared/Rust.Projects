c7739477e3a90cd592d262c7bbf5a333f1a05828
Jared Bruni <jaredbruni@gmail.com>
2022-09-03 01:09:46 -0700

    added url decode

--- source/to_html/src/bin/test-code.rs
+++ source/to_html/src/bin/test-code.rs
@@ -1,6 +1,8 @@
 use to_html::html::convert_to_html;
+use to_html::html::url_decode;
 
 fn main() -> std::io::Result<()> {
     println!("{}", convert_to_html("\"Hello World\" #include<iostream>"));
+    println!("{}", url_decode("Hello+World%5A"));
     Ok(())
 }
--- source/to_html/src/lib.rs
+++ source/to_html/src/lib.rs
@@ -1,5 +1,3 @@
-
-
 pub mod html {
 
     pub fn convert_to_html(input: &str) -> String {
@@ -29,5 +27,60 @@ pub mod html {
         s
     }
 
+    /*
+    std::string urldecode(std::string text) {
+        std::ostringstream stream;
+        for(unsigned int i = 0; i < text.length(); ++i) {
+            if(text[i] == '+') {
+                stream << " ";
+                continue;
+            }
+            if(text[i] == '%') {
+                if(i+2 < text.length()) {
+                    ++i;
+                    std::string test;
+                    test += text[i];
+                    ++i;
+                    test += text[i];
+                    int char_value;
+                    sscanf(test.c_str(), "%x", &char_value);
+                    stream << (char)char_value;
+                    continue;
+                }
+            }
+            stream << text[i];
+        }
+        return stream.str();
+    } */
 
+    pub fn url_decode(input: &str) -> String {
+        let mut s = String::new();
+        let mut i = 0;
+
+        while i < input.len() {
+            if input.chars().nth(i).unwrap() == '+' {
+                s.push(' ');
+                i += 1;
+                continue;
+            }
+            if input.chars().nth(i).unwrap() == '%' {
+                if i + 2 < input.len() {
+                    i += 1;
+                    let mut sval = String::new();
+                    sval.push(input.chars().nth(i).unwrap());
+                    i += 1;
+                    sval.push(input.chars().nth(i).unwrap());
+                    let ch_val = u8::from_str_radix(&sval, 16).unwrap();
+                    s.push(ch_val as char);
+                    i += 1;
+                    continue;
+                }
+                i += 1;
+                continue;
+            }
+            s.push(input.chars().nth(i).unwrap());
+            i += 1;
+        }
+        s
+    }
 }
