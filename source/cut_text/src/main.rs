f0936fff49b6f2beba01696672249efe6247f4dd
Jared Bruni <jaredbruni@gmail.com>
2022-09-06 13:06:48 -0700

    added substring

--- source/cut_text/src/main.rs
+++ source/cut_text/src/main.rs
@@ -4,6 +4,7 @@ struct Arguments {
     file: String,
     start: usize,
     number: usize,
+    mode: usize,
 }
 
 fn parse_args() -> Arguments {
@@ -25,14 +26,37 @@ fn parse_args() -> Arguments {
                 .help("cut start,number")
                 .multiple(false)
                 .allow_invalid_utf8(true)
-                .required(true)
+                .required(false)
                 .short('c')
-                .long("cut"),
+                .long("cut")
+                .conflicts_with("substr"),
+        )
+        .arg(
+            Arg::with_name("substr")
+                .value_name("SUBSTR")
+                .help("substring")
+                .multiple(false)
+                .allow_invalid_utf8(true)
+                .required(false)
+                .short('s')
+                .long("substr")
+                .conflicts_with("cut"),
         )
         .get_matches();
 
     let f = m.value_of_lossy("files").unwrap();
-    let s = m.value_of_lossy("cut").unwrap();
+    let s1 = m.value_of_lossy("cut");
+    let s2 = m.value_of_lossy("substr");
+    let s;
+    let mode_val;
+    if s1 != None {
+        s = s1.unwrap();
+        mode_val = 1;
+    } else {
+        s = s2.unwrap();
+        mode_val = 2;
+    }
+
     let pos = s.find(',').unwrap();
     let left = &s[..pos];
     let st: usize = left.parse().unwrap();
@@ -42,20 +66,26 @@ fn parse_args() -> Arguments {
         file: f.to_string(),
         start: st,
         number: n,
+        mode: mode_val,
     }
 }
 
-fn cut_value<T>(mut reader: T, start: usize, num: usize) -> String
+fn cut_value<T>(mut reader: T, start: usize, num: usize, mode: usize) -> String
 where
     T: std::io::BufRead + Sized,
 {
     let mut s = String::new();
     let _v = reader.read_to_string(&mut s).expect("on read");
-    cut_text(&s, start, num)
+    cut_text(&s, start, num, mode)
 }
 
-fn cut_text(input: &str, start: usize, num: usize) -> String {
-    let d = &input[start..start + num];
+fn cut_text(input: &str, start: usize, num: usize, mode: usize) -> String {
+    let d;
+    if mode == 1 {
+        d = &input[start..start + num];
+    } else {
+        d = &input[start..num];
+    }
     String::from(d)
 }
 
@@ -64,12 +94,12 @@ fn main() -> std::io::Result<()> {
     if args.file == "<STDIN>" {
         println!(
             "{}",
-            cut_value(std::io::stdin().lock(), args.start, args.number)
+            cut_value(std::io::stdin().lock(), args.start, args.number, args.mode)
         );
     } else {
         let f = std::fs::File::open(args.file)?;
         let r = std::io::BufReader::new(f);
-        println!("{}", cut_value(r, args.start, args.number));
+        println!("{}", cut_value(r, args.start, args.number, args.mode));
     }
     Ok(())
 }
