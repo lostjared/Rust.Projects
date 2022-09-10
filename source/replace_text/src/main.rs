dcbae61b5048b1402eb02295375b25d4b674fe4a
Jared Bruni <jaredbruni@gmail.com>
2022-09-10 15:14:00 -0700

    formatting

--- source/replace_text/src/main.rs
+++ source/replace_text/src/main.rs
@@ -3,7 +3,7 @@
 // to find each instance of search and replace it with replace in standard input stream
 // example:
 // cat src/main.rs | cargo run "/main/value"
-// or use 
+// or use
 // cat src/main.rs | cargo run "#main#value" --sep #
 // to change the character to seperate by
 
@@ -27,12 +27,21 @@ fn parse_args() -> Arguments {
                 .multiple(false)
                 .allow_invalid_utf8(true),
         )
-        .arg(Arg::with_name("sep").help("seperation character").required(false).multiple(false).long("sep").short('s').default_value("/").allow_invalid_utf8(true))
-
+        .arg(
+            Arg::with_name("sep")
+                .help("seperation character")
+                .required(false)
+                .takes_value(true)
+                .multiple(false)
+                .long("sep")
+                .short('s')
+                .default_value("/")
+                .allow_invalid_utf8(true),
+        )
         .get_matches();
     let t = m.value_of_lossy("text").unwrap();
     let sep = m.value_of_lossy("sep").unwrap().to_string();
-    let ch : char = sep.chars().nth(0).unwrap();
+    let ch: char = sep.chars().nth(0).unwrap();
     Arguments {
         text: t.to_string(),
         sep: ch,
@@ -58,7 +67,10 @@ fn main() -> std::io::Result<()> {
     let args = parse_args();
     let search_values = extract_search(&args.text, args.sep);
     if search_values == None {
-         return Err(std::io::Error::new(std::io::ErrorKind::Other, "Invalid text string"));
+        return Err(std::io::Error::new(
+            std::io::ErrorKind::Other,
+            "Invalid text string",
+        ));
     }
     let search_values = search_values.unwrap();
     for i in std::io::stdin().lock().lines() {
