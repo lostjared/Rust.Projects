5b9c4bbf4c29a80785ff77a248116c89b59e33ad
Jared Bruni <jaredbruni@gmail.com>
2022-09-20 10:47:05 -0700

    added negative numbers

--- source/rs_lex/src/bin/stack-int.rs
+++ source/rs_lex/src/bin/stack-int.rs
@@ -10,7 +10,7 @@ fn test_stack_math() {
 
 #[derive(Debug, PartialEq)]
 pub enum Input {
-    Digit(u64),
+    Digit(i64),
     Operator(char),
 }
 
@@ -36,7 +36,16 @@ fn print_stack(stack: &Vec<Input>) {
 }
 
 fn push_digits(v: &Vec<Box<dyn Token>>, index: &mut usize, stack: &mut Vec<Input>) {
-    if *index < v.len() && v[*index].get_type() == TokenType::Digits {
+
+    if *index < v.len() && v[*index].get_type() == TokenType::Symbol && v[*index].get_string() == "-" {
+        *index += 1;
+        let d : i64 = v[*index].get_string().parse().unwrap();
+        let num = Input::Digit(-d);
+        *index += 1;
+        stack.push(num);
+        push_digits(v, index, stack);
+    }
+    else if *index < v.len() && v[*index].get_type() == TokenType::Digits {
         stack.push(Input::Digit(v[*index].get_string().parse().unwrap()));
         *index += 1;
         push_digits(v, index, stack);
@@ -45,8 +54,8 @@ fn push_digits(v: &Vec<Box<dyn Token>>, index: &mut usize, stack: &mut Vec<Input
             '+' => {
                 let right = stack.pop().unwrap();
                 let left = stack.pop().unwrap();
-                let mut d1: u64 = 0;
-                let mut d2: u64 = 0;
+                let mut d1: i64 = 0;
+                let mut d2: i64 = 0;
                 match left {
                     Input::Digit(num) => {
                         d1 = num;
@@ -64,8 +73,8 @@ fn push_digits(v: &Vec<Box<dyn Token>>, index: &mut usize, stack: &mut Vec<Input
             '-' => {
                 let right = stack.pop().unwrap();
                 let left = stack.pop().unwrap();
-                let mut d1: u64 = 0;
-                let mut d2: u64 = 0;
+                let mut d1: i64 = 0;
+                let mut d2: i64 = 0;
                 match left {
                     Input::Digit(num) => {
                         d1 = num;
@@ -83,8 +92,8 @@ fn push_digits(v: &Vec<Box<dyn Token>>, index: &mut usize, stack: &mut Vec<Input
             '*' => {
                 let right = stack.pop().unwrap();
                 let left = stack.pop().unwrap();
-                let mut d1: u64 = 0;
-                let mut d2: u64 = 0;
+                let mut d1: i64 = 0;
+                let mut d2: i64 = 0;
                 match left {
                     Input::Digit(num) => {
                         d1 = num;
@@ -102,8 +111,8 @@ fn push_digits(v: &Vec<Box<dyn Token>>, index: &mut usize, stack: &mut Vec<Input
             '/' => {
                 let right = stack.pop().unwrap();
                 let left = stack.pop().unwrap();
-                let mut d1: u64 = 0;
-                let mut d2: u64 = 0;
+                let mut d1: i64 = 0;
+                let mut d2: i64 = 0;
                 match left {
                     Input::Digit(num) => {
                         d1 = num;
