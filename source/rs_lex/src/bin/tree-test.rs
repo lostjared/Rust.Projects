027af2c96646ce21dca36c0976495d06aeb7dbeb
Jared Bruni <jaredbruni@gmail.com>
2022-09-27 02:36:54 -0700

    tree

--- source/rs_lex/src/bin/tree-test.rs
+++ source/rs_lex/src/bin/tree-test.rs
@@ -1,9 +1,7 @@
 
 use rs_lex::rlex::tree::btree::*;
 
-
-fn main() -> std::io::Result<()> {
-
+fn tree_test() {
     let mut tree : Tree<i32> = Tree::new();
     let mut node : TreeNode<i32> = TreeNode::new(25);
     node.left = Some(Box::new(TreeNode::new(15)));
@@ -11,9 +9,16 @@ fn main() -> std::io::Result<()> {
     let node_right = Some(Box::new(TreeNode::new(200)));
     node.right = Some(Box::new(TreeNode::new_node(15, node_left, node_right)));
     tree.root = Some(Box::new(node));
-
     TreeNode::<i32>::print_nodes(&tree.root);
+}
 
+#[test]
+fn test_tree() {
+    tree_test();
+}
+
+fn main() -> std::io::Result<()> {
+    tree_test();
     Ok(())
 
 }
