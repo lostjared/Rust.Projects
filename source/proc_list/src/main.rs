7f4ab2d6abc258943f196fbf32bd9289345c42bb
Jared Bruni <jaredbruni@gmail.com>
2022-10-17 06:59:19 -0700

    updated to support stdin

--- source/proc_list/src/main.rs
+++ source/proc_list/src/main.rs
@@ -1,17 +1,16 @@
 // system call for list of text arguments
-use std::io::BufRead;
 use libc::system;
 use libc::c_char;
 use std::ffi::CString;
 
-fn proc_list(input: &str, cmd: &str) {
+fn proc_list<T>(reader: T, cmd: &str) 
+where
+    T: std::io::BufRead + Sized, {
     if cmd.find("%f") == None {
         eprintln!("Error required %f");
         return;
     }
-    let f = std::fs::File::open(input).expect("File open");
-    let r = std::io::BufReader::new(f);
-    for i in r.lines() {
+    for i in reader.lines() {
         match i {
             Ok(value) => {
                 let command = cmd.replace("%f", &value);
@@ -31,11 +30,15 @@ fn proc_list(input: &str, cmd: &str) {
 
 fn main() -> std::io::Result<()> {
     let args : Vec<String> = std::env::args().collect();
-    if args.len() != 3 {
+    if args.len() == 2 {
+        proc_list(std::io::stdin().lock(), &args[1]);
+    } else if args.len() == 3 {
+        let f = std::fs::File::open(&args[1])?;
+        let r = std::io::BufReader::new(f);
+        proc_list(r, &args[2]);
+    } else {
         eprintln!("Requires two arguments: input_list.txt \"command\"");
         return Ok(());
     }
-
-    proc_list(&args[1], &args[2]);
     Ok(())
 }
