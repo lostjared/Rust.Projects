ec1c8ad98ccbb7667f02e25f0a459a1dbf608c98
Jared Bruni <jaredbruni@gmail.com>
2022-10-26 07:37:36 -0700

    updated

--- source/knights_tour/src/main.rs
+++ source/knights_tour/src/main.rs
@@ -9,19 +9,43 @@ fn main() {
     let height = 720;
     let sdl = sdl2::init().unwrap();
     let video = sdl.video().unwrap();
-    let window = video.window("App", width, height).resizable().opengl().build().unwrap();
-    let mut can = window.into_canvas().build().map_err(|e| e.to_string()).expect("Error on canvas");
+    let window = video
+        .window("App", width, height)
+        .resizable()
+        .opengl()
+        .build()
+        .unwrap();
+    let mut can = window
+        .into_canvas()
+        .build()
+        .map_err(|e| e.to_string())
+        .expect("Error on canvas");
     let tc = can.texture_creator();
     let mut ksurf = Surface::load_bmp("knight.bmp").unwrap();
-    ksurf.set_color_key(true, sdl2::pixels::Color::RGB(255, 255, 255)).expect("on color key");
-    let knight = tc.create_texture_from_surface(ksurf).unwrap();   
+    ksurf
+        .set_color_key(true, sdl2::pixels::Color::RGB(255, 255, 255))
+        .expect("on color key");
+    let knight = tc.create_texture_from_surface(ksurf).unwrap();
     let mut e = sdl.event_pump().unwrap();
 
-    let mut curcolor : sdl2::pixels::Color;
-    let mut board : Box<[[u8; 8]; 8]> =  Box::new([[0; 8]; 8]); 
-    let mut kx = 0;
-    let mut ky = 0;
+    let mut curcolor: sdl2::pixels::Color;
+    let mut board: Box<[[u8; 8]; 8]> = Box::new([[0; 8]; 8]);
+    let mut rowx : i32 = 1;
+    let mut colx : i32 = 6;
+    let mut moves = 0;
+    let htable = [
+        [2, 3, 4, 4, 4, 4, 3, 2],
+        [3, 4, 6, 6, 6, 6, 4, 3],
+        [4, 6, 8, 8, 8, 8, 6, 4],
+        [4, 6, 8, 8, 8, 8, 6, 4],
+        [4, 6, 8, 8, 8, 8, 6, 4],
+        [4, 6, 8, 8, 8, 8, 6, 4],
+        [3, 4, 6, 6, 6, 6, 4, 3],
+        [2, 3, 4, 4, 4, 4, 3, 2],
+    ];
 
+    let horizontal = [2, 1, -1, -2, -2, -1, 1, 2];
+    let vertical = [-1, -2, -2, -1, 1, 2, 2, 1];
 
     'main: loop {
         for _event in e.poll_iter() {
@@ -31,53 +55,74 @@ fn main() {
                     keycode: Some(Keycode::Escape),
                     ..
                 } => break 'main,
+                Event::KeyDown {
+                    keycode: Some(Keycode::Space),
+                    ..
+                } => {
+                    let mut smallest = 100;
+                    let mut choice : i32 = -1;
+
+                    for i in 0..8 {
+                        let mut row;
+                        let mut col;
+                        row = rowx;
+                        col = colx;
+                        row += horizontal[i];
+                        col += vertical[i];
+                        if row >= 0 && row < 8 && col >= 0 && col < 8 && board[row as usize][col as usize] == 0 {
+                            if htable[row as usize][col as usize] < smallest && htable[row as usize][col as usize] != 0 {
+                                smallest = htable[row as usize][col as usize];
+                                choice = i as i32;
+                            }
+                        }
+                    }
+
+                    if choice != -1 {
+                        board[rowx as usize][colx as usize] = 1;
+                        rowx += horizontal[choice as usize] as i32;
+                        colx += vertical[choice as usize] as i32;
+                        moves += 1;
+                        if moves == 63 {
+                            moves += 1;
+                            board[rowx as usize][colx as usize] = 1;
+                        }
+                    }
+                }
                 _ => {}
             }
         }
-        can.set_draw_color(sdl2::pixels::Color::RGB(0,0,0));
+        can.set_draw_color(sdl2::pixels::Color::RGB(0, 0, 0));
         can.clear();
 
-        
-
         let startx = 100;
-	    let starty = 30;
-	    let mut counter = 0;
-	    let mut dx = startx;
-	    let mut dy = starty;
+        let starty = 30;
+        let mut counter = 0;
+        let mut dx = startx;
+        let mut dy = starty;
         let mut ion = 1;
 
-        for i in 0..8  {
+        for i in 0..8 {
             for z in 0..8 {
                 if ion == 1 {
                     curcolor = sdl2::pixels::Color::RGB(255, 255, 255);
-                }
-                else {
+                } else {
                     curcolor = sdl2::pixels::Color::RGB(255, 0, 0);
                 }
                 ion = !ion;
                 if board[i][z] == 0 {
                     can.set_draw_color(curcolor);
-                    can.fill_rect(Some(Rect::new(dx, dy, 50, 50))).expect("on fill");
+                    can.fill_rect(Some(Rect::new(dx, dy, 50, 50)))
+                        .expect("on fill");
                 }
 
-                if kx == i && ky == z {
-                    can.copy(
-                        &knight,
-                        None,
-                        Some(Rect::new(
-                           dx+5,
-                            dy+5,
-                            35,
-                            35,
-                        )),
-                    )
-                    .expect("copy tex");
+                if rowx == i as i32 && colx == z as i32 {
+                    can.copy(&knight, None, Some(Rect::new(dx + 5, dy + 5, 35, 35)))
+                        .expect("copy tex");
                 }
 
                 dx += 55;
                 counter += 1;
-                if counter >= 8
-                {
+                if counter >= 8 {
                     counter = 0;
                     dy += 55;
                     dx = startx;
@@ -85,7 +130,6 @@ fn main() {
                 }
             }
         }
-
 
         can.present();
     }
