1564fff414cc7cffce607f3763d87f17a4eba9cf
Jared Bruni <jaredbruni@gmail.com>
2022-10-28 04:34:50 -0700

    working on letters

--- source/matrix/src/main.rs
+++ source/matrix/src/main.rs
@@ -1,17 +1,16 @@
-
 // cargo run --release
 
+use rand::Rng;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::PixelFormatEnum;
 use sdl2::rect::Rect;
-use rand::Rng;
+use sdl2::render::TextureQuery;
 
+const LETTER_MAX: usize = 24;
+const LETTER_NUM: usize = 38;
 
-const LETTER_MAX: usize = 16;
-const LETTER_NUM: usize = 16;
-
-#[derive(Copy,Clone,Debug)]
+#[derive(Copy, Clone, Debug)]
 struct Letter {
     ch: char,
     xpos: i32,
@@ -19,33 +18,35 @@ struct Letter {
 }
 
 struct LetterGen {
-    letters: Vec<Box<[Letter; LETTER_MAX]>>
+    letters: Vec<Box<[Letter; LETTER_MAX]>>,
 }
 
 impl LetterGen {
-
     fn new() -> Self {
+        let mut rng = rand::thread_rng();
         let mut x = 0;
         let mut v = Vec::new();
         for i in 0..LETTER_NUM {
-            let  mut l = Box::new([Letter { ch: '0', xpos: x, ypos: 0}; LETTER_MAX]);
+            let mut l = Box::new(
+                [Letter {
+                    ch: '0',
+                    xpos: x,
+                    ypos: 0,
+                }; LETTER_MAX],
+            );
             let mut y = 0;
             for z in 0..LETTER_MAX {
-                l[z].ch = 'X';
+                l[z].ch = rng.gen_range('A'..='Z');
                 l[z].xpos = x;
                 l[z].ypos = y;
-                y += 16;
+                y += 32;
             }
             v.push(l);
-            x += 16;
+            x += 34;
         }
 
-        LetterGen {
-            letters: v,
-        }
-
+        LetterGen { letters: v }
     }
-
 }
 
 fn main() {
@@ -53,12 +54,21 @@ fn main() {
     let height = 720;
     let sdl = sdl2::init().unwrap();
     let video = sdl.video().unwrap();
-    let window = video.window("App", width, height).resizable().opengl().build().unwrap();
-    let mut can = window.into_canvas().build().map_err(|e| e.to_string()).expect("Error on canvas");
+    let window = video
+        .window("App", width, height)
+        .resizable()
+        .opengl()
+        .build()
+        .unwrap();
+    let mut can = window
+        .into_canvas()
+        .build()
+        .map_err(|e| e.to_string())
+        .expect("Error on canvas");
     let ttf_context = sdl2::ttf::init().map_err(|e| e.to_string()).unwrap();
     let tc = can.texture_creator();
     let font = ttf_context
-        .load_font("./font.ttf", 24)
+        .load_font("./font.ttf", 32)
         .expect("error loading font");
     let _text_surf = font
         .render("Hello, World!")
@@ -66,7 +76,7 @@ fn main() {
         .unwrap();
     let mut e = sdl.event_pump().unwrap();
     let mut rng = rand::thread_rng();
-    let mut letters = LetterGen::new();
+    let mut letters_st = LetterGen::new();
 
     'main: loop {
         for _event in e.poll_iter() {
@@ -80,6 +90,30 @@ fn main() {
             }
         }
         can.clear();
+
+        for i in 0..letters_st.letters.len() {
+            for z in 0..LETTER_MAX {
+                let ch = letters_st.letters[i][z].ch;
+                let x = letters_st.letters[i][z].xpos;
+                let y = letters_st.letters[i][z].ypos;
+
+                let text_surf = font
+                    .render(&format!("{}", ch))
+                    .blended(sdl2::pixels::Color::RGB(0, 255, 0))
+                    .unwrap();
+
+                let tex = tc.create_texture_from_surface(text_surf).unwrap();
+
+                let TextureQuery {
+                    width: wi,
+                    height: hi,
+                    ..
+                } = tex.query();
+
+                can.copy(&tex, None, Some(sdl2::rect::Rect::new(x, y, wi, hi)))
+                    .expect("on copy");
+            }
+        }
         can.present();
     }
 }
