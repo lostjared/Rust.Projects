4862b1bb89aeb6edcdf2a4d329c77a2e1f64b8f1
Jared Bruni <jaredbruni@gmail.com>
2022-10-28 04:17:02 -0700

    updated

--- source/matrix/src/main.rs
+++ source/matrix/src/main.rs
@@ -7,6 +7,47 @@ use sdl2::pixels::PixelFormatEnum;
 use sdl2::rect::Rect;
 use rand::Rng;
 
+
+const LETTER_MAX: usize = 16;
+const LETTER_NUM: usize = 16;
+
+#[derive(Copy,Clone,Debug)]
+struct Letter {
+    ch: char,
+    xpos: i32,
+    ypos: i32,
+}
+
+struct LetterGen {
+    letters: Vec<Box<[Letter; LETTER_MAX]>>
+}
+
+impl LetterGen {
+
+    fn new() -> Self {
+        let mut x = 0;
+        let mut v = Vec::new();
+        for i in 0..LETTER_NUM {
+            let  mut l = Box::new([Letter { ch: '0', xpos: x, ypos: 0}; LETTER_MAX]);
+            let mut y = 0;
+            for z in 0..LETTER_MAX {
+                l[z].ch = 'X';
+                l[z].xpos = x;
+                l[z].ypos = y;
+                y += 16;
+            }
+            v.push(l);
+            x += 16;
+        }
+
+        LetterGen {
+            letters: v,
+        }
+
+    }
+
+}
+
 fn main() {
     let width = 1280;
     let height = 720;
@@ -25,6 +66,8 @@ fn main() {
         .unwrap();
     let mut e = sdl.event_pump().unwrap();
     let mut rng = rand::thread_rng();
+    let mut letters = LetterGen::new();
+
     'main: loop {
         for _event in e.poll_iter() {
             match _event {
