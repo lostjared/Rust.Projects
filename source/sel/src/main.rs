db8d35a607044f1eb1a5c7589cf80e793b10194d
Jared Bruni <jaredbruni@gmail.com>
2022-11-19 10:17:44 -0800

    added sel

--- source/sel/src/main.rs
+++ source/sel/src/main.rs
@@ -1,3 +1,4 @@
+use std::io::Read;
 
 fn main() -> std::io::Result<()> {
     let args : Vec<String> = std::env::args().collect();
@@ -7,15 +8,29 @@ fn main() -> std::io::Result<()> {
         if sep == None {
             panic!("Missing required , ");
         }
+        let mut string_value : String = String::new();
+        let mut reader = std::io::stdin().lock();
+        reader.read_to_string(&mut string_value).expect("on read");
+
         let sep = sep.unwrap();
         let num1 = &val[0..sep];
         let num2 = &val[sep+1..];
-
-        let start_pos: usize = num1.parse().unwrap();
-        let stop_pos: usize = num2.parse().unwrap();
-
-        
-
+        let start_pos: usize;
+        let stop_pos: usize;
+        if num1 == "$" {
+            start_pos = 0;
+        } else {
+            start_pos = num1.parse().unwrap();
+        }
+        if num2 == "$" {
+            stop_pos = string_value.len();
+        } else {
+            stop_pos = num2.parse().unwrap();
+        }
+        if start_pos < string_value.len() && stop_pos > start_pos {
+            let cut_value = &string_value[start_pos..stop_pos];
+            println!("{}", cut_value);
+        }
     }
     Ok(())
 }
