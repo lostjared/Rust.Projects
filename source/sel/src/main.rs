6671b7842b397b7bc30d3c39ca6c09ab7c31e136
Jared Bruni <jaredbruni@gmail.com>
2022-11-20 11:36:17 -0800

    Formatting

--- source/sel/src/main.rs
+++ source/sel/src/main.rs
@@ -3,17 +3,22 @@ use std::io::Read;
 fn main() -> std::io::Result<()> {
     let args: Vec<String> = std::env::args().collect();
     if args.len() == 2 {
+
         let val = &args[1];
         let sep = val.find(',');
+
         if sep == None {
             panic!("Missing required argument , ");
         }
+
         let mut string_value: String = String::new();
         let mut reader = std::io::stdin().lock();
         reader.read_to_string(&mut string_value).expect("on read");
+
         let sep = sep.unwrap();
         let num1 = &val[0..sep];
         let num2 = &val[sep + 1..];
+
         let start_pos: usize = if num1 == "$" {
             0
         } else {
