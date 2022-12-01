0cff11eba61e06a6c1770aca45d9608db4326130
Jared Bruni <jaredbruni@gmail.com>
2022-12-01 11:32:39 -0800

    formatting

--- source/sel/src/main.rs
+++ source/sel/src/main.rs
@@ -33,42 +33,42 @@ fn parse_args() -> Arguments {
 }
 
 fn main() -> std::io::Result<()> {
-        let args = parse_args();
-        let val = args.r;
-        let sep = val.find(',');
+    let args = parse_args();
+    let val = args.r;
+    let sep = val.find(',');
 
-        if sep == None {
-            panic!("Missing required argument , ");
-        }
+    if sep == None {
+        panic!("Missing required argument , ");
+    }
 
-        let mut string_value: String = String::new();
-        let mut reader = std::io::stdin().lock();
-        reader.read_to_string(&mut string_value).expect("on read");
+    let mut string_value: String = String::new();
+    let mut reader = std::io::stdin().lock();
+    reader.read_to_string(&mut string_value).expect("on read");
 
-        let sep = sep.unwrap();
-        let num1 = &val[0..sep];
-        let num2 = &val[sep + 1..];
+    let sep = sep.unwrap();
+    let num1 = &val[0..sep];
+    let num2 = &val[sep + 1..];
 
-        let start_pos: usize = if num1 == "$" {
-            0
+    let start_pos: usize = if num1 == "$" {
+        0
+    } else {
+        num1.parse().unwrap()
+    };
+
+    let stop_pos: usize = if num2 == "$" {
+        string_value.len()
+    } else {
+        num2.parse().unwrap()
+    };
+
+    if start_pos < string_value.len() && stop_pos > start_pos {
+        if !args.mid {
+            let cut_value = &string_value[start_pos..stop_pos];
+            println!("{}", cut_value);
         } else {
-            num1.parse().unwrap()
-        };
-
-        let stop_pos: usize = if num2 == "$" {
-            string_value.len()
-        } else {
-            num2.parse().unwrap()
-        };
-
-        if start_pos < string_value.len() && stop_pos > start_pos {
-            if !args.mid  {
-                let cut_value = &string_value[start_pos..stop_pos];
-                println!("{}", cut_value);
-            } else {
-                let cut_value = &string_value[start_pos..start_pos+stop_pos];
-                println!("{}", cut_value);
-            }
+            let cut_value = &string_value[start_pos..start_pos + stop_pos];
+            println!("{}", cut_value);
         }
-       Ok(())
+    }
+    Ok(())
 }
