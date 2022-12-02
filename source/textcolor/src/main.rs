e3f171bf1fc47f0be3073f9ffc45b33f442ad6f9
Jared Bruni <jaredbruni@gmail.com>
2022-12-02 12:57:54 -0800

    using color

--- source/textcolor/src/main.rs
+++ source/textcolor/src/main.rs
@@ -1,7 +1,7 @@
 use colored::Colorize;
 
-fn main() {
-
+fn main() -> std::io::Result<()> {
     println!("{}: is red", "Color".red());
-
+    println!("{}: is blue bold", "Color2".blue().bold());
+    Ok(())
 }
