f9d438d67a2f4af0c6330ecb7ea5a822a88c328b
Jared Bruni <jaredbruni@gmail.com>
2023-02-08 20:09:09 -0800

    added list processing

--- source/img_resize/src/main.rs
+++ source/img_resize/src/main.rs
@@ -12,6 +12,7 @@ struct Arguments {
     outfile: String,
     size_val: (u32, u32),
     exact: bool,
+    list: String,
 }
 /// parse arguments
 fn parse_args() -> Arguments {
@@ -25,6 +26,7 @@ fn parse_args() -> Arguments {
                 .long("input")
                 .takes_value(true)
                 .required(true)
+                .conflicts_with("list")
                 .allow_invalid_utf8(true),
         )
         .arg(
@@ -33,7 +35,9 @@ fn parse_args() -> Arguments {
                 .short('o')
                 .long("output")
                 .takes_value(true)
+                .conflicts_with("list")
                 .required(true)
+                .default_value("<NULL>")
                 .allow_invalid_utf8(true),
         )
         .arg(
@@ -53,41 +57,103 @@ fn parse_args() -> Arguments {
                 .takes_value(false)
                 .required(false),
         )
+        .arg(
+            Arg::new("list")
+                .help("process list")
+                .short('l')
+                .long("list")
+                .takes_value(true)
+                .required(false)
+                .default_value("<NULL>")
+                .allow_invalid_utf8(true),
+        )
         .get_matches();
 
-    let input_value = m.value_of_lossy("input").unwrap();
-    let output_value = m.value_of_lossy("output").unwrap();
-    let res = m.value_of_lossy("res").unwrap();
+    let ls = m.value_of_lossy("list").unwrap();
 
-    let f = res.find('x').unwrap();
-    let sx = &res[..f];
-    let sy = &res[f + 1..];
-    let size_value = (sx.parse().unwrap(), sy.parse().unwrap());
-    let exact_value = m.is_present("exact");
-    Arguments {
-        infile: input_value.to_string(),
-        outfile: output_value.to_string(),
-        size_val: size_value,
-        exact: exact_value,
+    if ls.to_string() != "<NULL>" {
+        let res = m.value_of_lossy("res").unwrap();
+
+        let f = res.find('x').unwrap();
+        let sx = &res[..f];
+        let sy = &res[f + 1..];
+        let size_value = (sx.parse().unwrap(), sy.parse().unwrap());
+        let exact_value = m.is_present("exact");
+        Arguments {
+            infile: "<NULL>".to_string(),
+            outfile: "<NULL>".to_string(),
+            size_val: size_value,
+            exact: exact_value,
+            list: ls.to_string(),
+        }
+    } else {
+        let input_value = m.value_of_lossy("input").unwrap();
+        let output_value = m.value_of_lossy("output").unwrap();
+        let res = m.value_of_lossy("res").unwrap();
+
+        let f = res.find('x').unwrap();
+        let sx = &res[..f];
+        let sy = &res[f + 1..];
+        let size_value = (sx.parse().unwrap(), sy.parse().unwrap());
+        let exact_value = m.is_present("exact");
+
+        Arguments {
+            infile: input_value.to_string(),
+            outfile: output_value.to_string(),
+            size_val: size_value,
+            exact: exact_value,
+            list: ls.to_string(),
+        }
     }
+}
+
+fn build_list(input: &str) -> Vec<String> {
+    let data = std::fs::read_to_string(input).unwrap();
+    let mut v: Vec<String> = Vec::new();
+    for file in data.lines() {
+        v.push(file.to_string());
+    }
+    v
+}
+
+fn convert_file(infile: &str, outfile: &str, size_val: (u32, u32), exact: bool) {
+    let i = image::open(infile).unwrap();
+    let resized = if exact {
+        i.resize_exact(size_val.0, size_val.1, image::imageops::Lanczos3)
+    } else {
+        i.resize(size_val.0, size_val.1, image::imageops::Lanczos3)
+    };
+    resized.save(outfile).expect("Error on save");
+    println!(
+        "{} -> {} : {}x{}",
+        infile.red(),
+        outfile.blue(),
+        resized.width(),
+        resized.height()
+    );
 }
 
 /// main function
 fn main() -> std::io::Result<()> {
     let args = parse_args();
-    let i = image::open(&args.infile).unwrap();
-    let resized = if args.exact {
-        i.resize_exact(args.size_val.0, args.size_val.1, image::imageops::Lanczos3)
+    if args.list == "<NULL>" {
+        convert_file(&args.infile, &args.outfile, args.size_val, args.exact);
     } else {
-        i.resize(args.size_val.0, args.size_val.1, image::imageops::Lanczos3)
-    };
-    resized.save(&args.outfile).expect("Error on save");
-    println!(
-        "{} -> {} : {}x{}",
-        args.infile.red(),
-        args.outfile.blue(),
-        resized.width(),
-        resized.height()
-    );
+        let ls = build_list(&args.list);
+        for i in ls {
+            let ext = std::path::Path::new(&i).extension().unwrap();
+
+            let path = i.find(&ext.to_string_lossy().to_string()).unwrap();
+            let lpath = &i[..path];
+            let new_f = format!(
+                "{}{}x{}.{}",
+                lpath,
+                args.size_val.0,
+                args.size_val.1,
+                ext.to_string_lossy()
+            );
+            convert_file(&i, &new_f, args.size_val, args.exact);
+        }
+    }
     Ok(())
 }
