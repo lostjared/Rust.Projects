ecf774ad16dd54ef9b51c9f3f8cf50b78041d3a6
Jared Bruni <jaredbruni@gmail.com>
2024-04-20 21:10:33 -0700

    updated parameter passing

--- source/julia/src/main.rs
+++ source/julia/src/main.rs
@@ -1,12 +1,10 @@
-use cmd_switch::cmd_sw;
-use std::collections::HashMap;
-use std::env;
+use rand::Rng;
 
 pub fn parse_split_int(t: &str) -> (i32, i32) {
     let text = String::from(t.trim());
-    let pos = text.find(',');
+    let pos = text.find('x');
     if pos == None {
-        panic!("Could not find list seperator for argument..");
+        panic!("Could not find list seperator for argument!..");
     }
     let pos_value = pos.unwrap();
     let left = &text[0..pos_value];
@@ -27,51 +25,26 @@ pub fn parse_split_double(t: &str) -> (f32, f32) {
 }
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let mut desc = HashMap::new();
-
-    desc.insert(String::from("output"), (String::from("output file"), true));
-    desc.insert(
-        String::from("res"),
-        (String::from("output resolution"), true),
-    );
-    desc.insert(String::from("param"), (String::from("complex pair"), true));
-    desc.insert(
-        String::from("iter"),
-        (String::from("number of iterations"), true),
-    );
-
-    if args.len() == 1 {
-        cmd_sw::print_accepted_args_map_require(&desc);
-        std::process::exit(0);
-    }
-    let argx = cmd_sw::parse_args_require(&args, &desc);
-    match argx {
-        Ok(argz) => {
-            let output = &argz["output"];
-            let res = &argz["res"];
-            let res_value = parse_split_int(&res.value);
-            let param = &argz["param"];
-            let iter = &argz["iter"];
-            let iterations = iter.value.parse::<i32>().unwrap();
-            let params = parse_split_double(&param.value);
-
-            println!(
-                "output: [{}] resolution: ({}, {}) param: ({}, {}) iterations: {}",
-                output.value, res_value.0, res_value.1, params.0, params.1, iterations
-            );
-            draw_julia(&output.value, res_value, params, iterations);
-        }
-        Err(e) => {
-            println!("Error: {}", e);
-        }
-    }
+    println!("output.png 800x600 paramA,paramB iter");
+    let args : Vec<String> = std::env::args().collect();
+    let res = parse_split_int(&args[2]);
+    let param = parse_split_double(&args[3]);
+    let iter = args[4].parse::<i32>();
+    draw_julia(&args[1], res, param, iter.unwrap());
 }
 
 pub fn draw_julia(filename: &str, res: (i32, i32), param: (f32, f32), iter: i32) {
-    // from image crate example website
     let imgx = res.0 as u32;
     let imgy = res.1 as u32;
+    let mut rng = rand::thread_rng();
+    let mut color_map : Vec<(u8, u8, u8)> = Vec::new();
+    for _i in 0..=iter {
+        let r = rng.gen::<u8>(); 
+        let g = rng.gen::<u8>(); 
+        let b = rng.gen::<u8>(); 
+        color_map.push((r, g, b));
+    }
+
 
     let scalex = 3.0 / imgx as f32;
     let scaley = 3.0 / imgy as f32;
@@ -93,8 +66,9 @@ pub fn draw_julia(filename: &str, res: (i32, i32), param: (f32, f32), iter: i32)
                 i += 1;
             }
             let pixel = imgbuf.get_pixel_mut(x, y);
-            let image::Rgb(data) = *pixel;
-            *pixel = image::Rgb([data[0], i as u8, data[2]]);
+            let image::Rgb(_data) = *pixel;
+            let c = color_map[i as usize];
+            *pixel = image::Rgb([c.0, c.1, c.2]);
         }
     }
     imgbuf.save(&filename).unwrap();
