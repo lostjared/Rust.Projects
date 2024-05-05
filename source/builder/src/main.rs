2e15106fbb6236dc39bf318c2021044082e98c88
Jared Bruni <jaredbruni@gmail.com>
2024-05-05 14:47:30 -0700

    builder example

---
+++ source/builder/src/main.rs
@@ -0,0 +1,53 @@
+struct RType {
+    pub data: String,
+    pub index: u32,
+}
+
+impl RType {
+    pub fn new() -> RType {
+        RType {
+            data: String::new(),
+            index: 0,
+        }
+    }
+}
+
+struct RTypeBuilder {
+    data: Option<String>,
+    index: u32,
+}
+
+impl RTypeBuilder {
+    fn new() -> Self {
+        Self {
+            data: None,
+            index: 0,
+        }
+    }
+
+    fn set_text(mut self, text: String) -> Self {
+        self.data = Some(text);
+        self
+    }
+
+    fn set_index(mut self, index: u32) -> Self {
+        self.index = index;
+        self
+    }
+
+    fn build(self) -> RType {
+        RType {
+            data: self.data.expect("data not found"),
+            index: self.index,
+        }
+    }
+}
+
+fn main() -> std::io::Result<()> {
+    let rtype = RTypeBuilder::new()
+        .set_text("Hello, World".to_string())
+        .set_index(10)
+        .build();
+    println!("data: {}, index: {}", rtype.data, rtype.index);
+    Ok(())
+}
