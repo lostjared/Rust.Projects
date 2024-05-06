528065513d331d7acd5462a0ccccc825df44a80b
Jared Bruni <jaredbruni@gmail.com>
2024-05-06 02:31:15 -0700

    karma2

---
+++ source/karma2/src/main.rs
@@ -0,0 +1,41 @@
+use std::io::{BufRead, Write};
+
+fn give<I, O>(input: &mut I, output: &mut O) -> String
+where
+    I: BufRead,
+    O: Write,
+{
+    input_line("Enter something: ", input, output)
+}
+fn get(text: String) -> Option<String> {
+    if text == "death" {
+        return None;
+    }
+    Some(text)
+}
+fn input_line<I, O>(text: &str, input: &mut I, output: &mut O) -> String
+where
+    I: BufRead,
+    O: Write,
+{
+    write!(output, "{}", text).expect("Error on write");
+    output.flush().expect("Error on flush");
+    let mut s = String::new();
+    input.read_line(&mut s).expect("Error on Read line");
+    s.trim_end().to_string()
+}
+
+fn main() -> std::io::Result<()> {
+    let mut input = std::io::stdin().lock();
+    let mut output = std::io::stdout().lock();
+    loop {
+        let data = get(give(&mut input, &mut output));
+        if let Some(data) = data {
+            println!("Karma Returns: {}", data);
+        } else {
+            break;
+        }
+    }
+    println!("Eescape!");
+    Ok(())
+}
