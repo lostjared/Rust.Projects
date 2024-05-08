1da1fca230160103824725fde04b7f27150dce97
Jared Bruni <jaredbruni@gmail.com>
2024-05-08 09:21:03 -0700

    updated

--- source/game-crate/mxr/src/bin/main.rs
+++ source/game-crate/mxr/src/bin/main.rs
@@ -3,8 +3,8 @@ use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 
 fn main() -> std::io::Result<()> {
-    let mut obj = WindowBuilder::new().create("Hello World", 640, 480).build();
-    let mut e = obj.sdl.event_pump().unwrap();
+    let mut mx = WindowBuilder::new().create("Hello World", 640, 480).build();
+    let mut e = mx.sdl.event_pump().unwrap();
     'main: loop {
         for _event in e.poll_iter() {
             match _event {
@@ -16,9 +16,9 @@ fn main() -> std::io::Result<()> {
                 _ => {}
             }
         }
-        obj.can.set_draw_color(sdl2::pixels::Color::RGB(0, 0, 0));
-        obj.can.clear();
-        obj.can.present();
+        mx.can.set_draw_color(sdl2::pixels::Color::RGB(0, 0, 0));
+        mx.can.clear();
+        mx.can.present();
     }
     Ok(())
 }
