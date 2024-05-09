4b0401d91d2d6523a6a29af2d437db5b67c1fb09
Jared Bruni <jaredbruni@gmail.com>
2024-05-09 06:34:07 -0700

    load graphics function

---
+++ source/game-crate/mxr/src/bin/test2.rs
@@ -0,0 +1,53 @@
+use mxr::mxr::*;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+
+fn load_gfx<'a>(mx: &mut Window, tc: &'a sdl2::render::TextureCreator<sdl2::video::WindowContext>) -> Result<Vec<sdl2::render::Texture<'a>>, String> {
+    let file_strings = vec!["./data/logo.bmp"];
+    let mut v = Vec::new();
+    for i in file_strings {
+        let t = mx.load_texture(&tc, i)?;
+        v.push(t);
+    }
+    Ok(v)
+}
+
+fn main() -> Result<(), String> {
+    let mut mx = WindowBuilder::new()
+        .create("Hello World [load_gfx]", 1440, 1080)
+        .build()?;
+    let ttf_context = sdl2::ttf::init().map_err(|e| e.to_string())?;
+    let font = ttf_context.load_font("./data/font.ttf", 18)?;
+    let tc = mx.can.texture_creator();
+    let textures = load_gfx(&mut mx, &tc)?;
+    let mut frame_counter: u64 = 0;
+
+    'main: loop {
+        for event in mx.event.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'main,
+                _ => {}
+            }
+        }
+        mx.can.set_draw_color(sdl2::pixels::Color::RGB(0, 0, 0));
+        mx.can.clear();
+        mx.can
+            .copy(&textures[0], None, None)
+            .expect("Failure to copy texture to canvas");
+        mx.printtext(
+            &font,
+            15,
+            15,
+            sdl2::pixels::Color::RGB(255, 255, 255),
+            &format!("load_gfx() test {} frames drawn", frame_counter),
+        )
+        .expect("Failure to print text to screen.");
+        mx.can.present();
+        frame_counter += 1;
+    }
+    Ok(())
+}
