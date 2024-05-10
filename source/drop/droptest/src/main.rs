42ce34ae084f2520be09ea92d42e1eadd822ed40
Jared Bruni <jaredbruni@gmail.com>
2024-05-10 05:06:17 -0700

    added drop ex to remember

--- source/drop/droptest/src/main.rs
+++ source/drop/droptest/src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+
+struct Type {
+    name: String,
+    id: u32,
+}
+
+impl Type {
+    fn new() -> Self {
+        Self {
+            name: "Jared".to_string(),
+            id: 250
+        }
+    }
+    fn set_id(&mut self, i: u32) {
+        self.id = i;
+    }
+}
+
+impl Drop for Type {
+    fn drop(&mut self)  {
+        println!("Dropping {}:{}", self.name, self.id);
+        // clean up here same as destructor in C++
+    }
+}
+
+fn main() -> std::io::Result<()> {
+    let mut t = Type::new();
+    t.set_id(100);
+    Ok(())
 }
