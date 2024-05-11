2f6f7eebb9075d04375f006f9d12d0a9f8a71224
Jared Bruni <jaredbruni@gmail.com>
2024-05-10 22:06:07 -0700

    pixels drawn

--- source/game-crate/mxr/src/bin/pixels.rs
+++ source/game-crate/mxr/src/bin/pixels.rs
@@ -1,18 +1,28 @@
+
+// texture sized at 640x480
+// random pixels drawn
+// scaled to 1440x1080 or arguments: width height
+
 use mxr::mxr::*;
 use rand::Rng;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 fn main() -> Result<(), String> {
-    let width = 1440;
-    let height = 1080;
+    let mut width = 1440;
+    let mut height = 1080;
+    let args : Vec<String> = std::env::args().collect();
+    if args.len() == 3 {
+        width = args[1].parse::<u32>().unwrap();
+        height = args[2].parse::<u32>().unwrap();
+    }
     let mut mx = MXWindowBuilder::new()
-        .create("Hello World - [Random Pixels]", 1440, 1080)
+        .create("Hello World - [Random Pixels]", width, height)
         .build()?;
     let ttf_context = sdl2::ttf::init().map_err(|e| e.to_string())?;
     let font = ttf_context.load_font("./data/font.ttf", 18)?;
     let tc = mx.can.texture_creator();
     let mut texture = tc
-        .create_texture_streaming(sdl2::pixels::PixelFormatEnum::RGB24, width, height)
+        .create_texture_streaming(sdl2::pixels::PixelFormatEnum::RGB24,640, 480)
         .map_err(|e| e.to_string())?;
     let mut frame_counter: u64 = 0;
     'main: loop {
@@ -28,10 +38,11 @@ fn main() -> Result<(), String> {
         }
         mx.can.set_draw_color(sdl2::pixels::Color::RGB(0, 0, 0));
         mx.can.clear();
+        let tex_size = tex_get_size(&texture);
         texture.with_lock(None, |buffer: &mut [u8], pitch: usize| {
             let mut rng = rand::thread_rng();
-            for y in 0..height {
-                for x in 0..width {
+            for y in 0..tex_size.1 {
+                for x in 0..tex_size.0 {
                     let offset = x as usize * 3 + y as usize * pitch;
                     buffer[offset as usize] = rng.gen_range(0..256) as u8;
                     buffer[offset as usize + 1] = rng.gen_range(0..256) as u8;
