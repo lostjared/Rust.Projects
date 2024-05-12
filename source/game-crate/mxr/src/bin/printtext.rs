a69d502de24e03c6f305de18d7030d836cdc82a0
Jared Bruni <jaredbruni@gmail.com>
2024-05-12 00:14:31 -0700

    formatting

--- source/game-crate/mxr/src/bin/printtext.rs
+++ source/game-crate/mxr/src/bin/printtext.rs
@@ -9,7 +9,12 @@ fn main() -> Result<(), String> {
     let ttf_context = sdl2::ttf::init().map_err(|e| e.to_string())?;
     let font = ttf_context.load_font("./data/font.ttf", 32)?;
     let tc = mx.can.texture_creator();
-    let tex = mx.printtext_texture(&font, &tc, sdl2::pixels::Color::RGB(255, 0, 0), "Generated at start up")?;
+    let tex = mx.printtext_texture(
+        &font,
+        &tc,
+        sdl2::pixels::Color::RGB(255, 0, 0),
+        "Generated at start up",
+    )?;
     let tex_w = tex_get_size(&tex);
     let mut frame_counter: u64 = 0;
     'main: loop {
@@ -32,7 +37,11 @@ fn main() -> Result<(), String> {
             sdl2::pixels::Color::RGB(255, 255, 255),
             &format!("Hello, World! {} frames drawn", frame_counter),
         )?;
-        mx.can.copy(&tex, None, sdl2::rect::Rect::new(100, 100, tex_w.0, tex_w.1))?;
+        mx.can.copy(
+            &tex,
+            None,
+            sdl2::rect::Rect::new(100, 100, tex_w.0, tex_w.1),
+        )?;
         mx.can.present();
         frame_counter += 1;
     }
