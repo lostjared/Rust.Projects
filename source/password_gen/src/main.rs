011bc977dfe600f05f428c865cbb21426b1f4b37
Jared Bruni <jaredbruni@gmail.com>
2024-05-15 06:46:00 -0700

    formatting

--- source/password_gen/src/main.rs
+++ source/password_gen/src/main.rs
@@ -1,6 +1,5 @@
-
-use rand::Rng;
 use rand::rngs::ThreadRng;
+use rand::Rng;
 use shuffle::irs::Irs;
 use shuffle::shuffler::Shuffler;
 
@@ -9,20 +8,19 @@ pub enum CharType {
     UpperLetter,
     LowerLetter,
     Digit,
-    Symbol
+    Symbol,
 }
 
 fn generate_char(rng: &mut ThreadRng, ch_type: &CharType) -> char {
-
     match ch_type {
         CharType::UpperLetter => {
-            return rng.gen_range('A' as u8 ..= 'Z' as u8) as char;
+            return rng.gen_range('A' as u8..='Z' as u8) as char;
         }
         CharType::LowerLetter => {
-            return rng.gen_range('a' as u8 ..= 'z' as u8) as char;
+            return rng.gen_range('a' as u8..='z' as u8) as char;
         }
         CharType::Digit => {
-            return rng.gen_range('0' as u8 ..= '9' as u8) as char;
+            return rng.gen_range('0' as u8..='9' as u8) as char;
         }
         CharType::Symbol => {
             let valid = String::from("!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~");
@@ -32,12 +30,11 @@ fn generate_char(rng: &mut ThreadRng, ch_type: &CharType) -> char {
     }
 }
 
-
 fn generate_password(length: u32) -> String {
-    let mut value : Vec<char> = Vec::new();
+    let mut value: Vec<char> = Vec::new();
     let mut rng = rand::thread_rng();
     for _i in 0..length * 2 {
-        let rtype =  rng.gen_range(0..4);
+        let rtype = rng.gen_range(0..4);
         match rtype {
             0 => value.push(generate_char(&mut rng, &CharType::UpperLetter)),
             1 => value.push(generate_char(&mut rng, &CharType::LowerLetter)),
@@ -48,21 +45,22 @@ fn generate_password(length: u32) -> String {
     }
     let mut shuffle_rand = rand::thread_rng();
     let mut irs = Irs::default();
-    irs.shuffle(&mut value, &mut shuffle_rand).expect("on shuffle");
+    irs.shuffle(&mut value, &mut shuffle_rand)
+        .expect("on shuffle");
     let mut pw = String::new();
-    for i  in 0..length  {
+    for i in 0..length {
         pw.push(value[i as usize]);
     }
     pw
 }
 
 fn main() {
-    let args : Vec<String> = std::env::args().collect();
+    let args: Vec<String> = std::env::args().collect();
     if args.len() != 2 {
         println!("argument: length of password");
         std::process::exit(1);
     }
     let len = args[1].parse::<u32>().unwrap();
-    let password = generate_password(len);  
+    let password = generate_password(len);
     println!("generated password: {}", password);
 }
