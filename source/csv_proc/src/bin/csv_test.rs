90ddb22ec4fe7238e96a8c03522fcfde4c13ae9d
Jared Bruni <jaredbruni@gmail.com>
2024-05-16 19:38:14 -0700

    Update csv_test.rs

--- source/csv_proc/src/bin/csv_test.rs
+++ source/csv_proc/src/bin/csv_test.rs
@@ -1,28 +1,13 @@
-// Like a CSV file but with syntax of a programming language
-// called a csv_proc
-// each row/col contains one Token either an Idenetifier, String, Digits, Symbol, or Single String
-// to allow more than one type surround in quotes
-
-use csv_proc::csv::*;
+use csv_proc::xcsv::*;
 
 fn main() -> std::io::Result<()> {
     let args: Vec<String> = std::env::args().collect();
     if args.len() != 2 {
-        eprintln!("Error requires argument file to proc");
+        eprintln!("incorrect arguments");
         std::process::exit(1);
     }
-    let mut csv = Csv::new();
-    csv.load_file(&args[1], ",")?;
-    for row in &csv.data {
-        let size = row.len();
-        if size > 0 {
-            print!("ROW [");
-            for i in &*row {
-                print!(" {} ", i);
-            }
-            println!("]");
-        }
-    }
-    println!(" row 0 col 1 {}", csv.data[0][1]);
+    let mut xcsv = XCsv::new();
+    xcsv.load_file(&args[1], &',')?;
+    xcsv.save_file("test1.txt", &',')?;
     Ok(())
 }
