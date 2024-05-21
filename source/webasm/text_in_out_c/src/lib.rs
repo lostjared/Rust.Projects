7e81cc3b13a2c758e9a805e647955e9a7112d5cd
Jared Bruni <jaredbruni@gmail.com>
2024-05-21 10:30:14 -0700

    updated

--- source/webasm/text_in_out_c/src/lib.rs
+++ source/webasm/text_in_out_c/src/lib.rs
@@ -4,36 +4,32 @@ use std::io::BufReader;
 #[wasm_bindgen]
 pub fn convert_text(text: &str) -> String {
     let reader = BufReader::new(text.as_bytes());
-    convert_to_rs(reader)
+    convert_to_cxx(reader, true)
 }
 
-fn convert_to_rs<T>(mut reader: T) -> String
-where
-    T: std::io::BufRead + Sized,
-{
+fn convert_to_cxx<T: std::io::BufRead + Sized>(mut reader: T, name: &str, blank: bool) -> String {
+    use std::fmt::Write;
     let mut value: String = String::new();
-    value.push_str("let v : Vec<u8> = vec![");
-    let mut bytes = [0; 1024];
-    let mut first_byte = true;
-
+    write!(&mut value, "std::vector<std::string> {} {{", name).expect("on write");
     loop {
-        let val = reader.read(&mut bytes).expect("Failed to read");
-        if val == 0 {
-            break;
+        let mut input_text: String = String::new();
+        let val = reader.read_line(&mut input_text).expect("on read");
+        if input_text == "\n" && blank == true {
+            continue;
         }
-        if !first_byte {
-            value.push(',');
-        }
-        first_byte = false;
-
-        for i in 0..val {
-            if i > 0 {
+        input_text.pop();
+        if input_text.len() > 0 || blank == false {
+            write!(&mut value, "\n\"{}\"", &slash_seq(&input_text)).expect("on write");
+            if val == 0 {
+                break;
+            } else {
                 value.push(',');
             }
-            let s = format!("{:#04x}", bytes[i]);
-            value.push_str(&s);
+        } else if val == 0 {
+            value.remove(value.len() - 1);
+            break;
         }
     }
-    value.push_str("];\n");
+    value.push_str("\n};\n");
     value
 }
