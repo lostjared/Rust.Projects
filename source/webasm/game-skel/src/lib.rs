f26a573d4ae1ddd61e062e1d5e6868361014d5e0
Jared Bruni <jaredbruni@gmail.com>
2024-05-24 10:43:22 -0700

    init

---
+++ source/webasm/game-skel/src/lib.rs
@@ -0,0 +1,33 @@
+
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn hello_world() -> String {
+    "hello, world".to_string()
+}
+
+#[wasm_bindgen]
+struct Object {
+    x: i32,
+}
+
+#[wasm_bindgen]
+impl Object  {
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> Self {
+        Object {
+            x: 0
+        }
+
+    }
+    
+    #[wasm_bindgen(getter)]
+    ub fn x(&self) -> i32 {
+        self.x
+    }
+\   #[wasm_bindgen(setter)]
+    pub fn x(&self, value: i32) {
+        self.x = value;
+    }
+
+}
